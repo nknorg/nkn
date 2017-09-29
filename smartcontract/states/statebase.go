18237850db037456a21978b4491aba8e6e1fa54d
tanyuan <1067598718@qq.com>
2017-09-28 22:04:05 -0500

    remove pointer, struct only have a attribute.

--- smartcontract/states/statebase.go
+++ smartcontract/states/statebase.go
@@ -10,12 +10,12 @@ type StateBase struct {
 	StateVersion byte
 }
 
-func(stateBase *StateBase)Serialize(w io.Writer) error {
+func(stateBase StateBase)Serialize(w io.Writer) error {
 	serialization.WriteByte(w, stateBase.StateVersion)
 	return nil
 }
 
-func(stateBase *StateBase)Deserialize(r io.Reader) error {
+func(stateBase StateBase)Deserialize(r io.Reader) error {
 	stateVersion, err := serialization.ReadByte(r)
 	if err != nil {
 		return errors.NewDetailErr(err, errors.ErrNoCode, "StateBase StateVersion Deserialize fail.")
