418aece5028bb65e913d0068dab79375a2f3b411
tanyuan <1067598718@qq.com>
2017-09-29 04:51:10 -0500

    add version control

--- smartcontract/states/validatorstate.go
+++ smartcontract/states/validatorstate.go
@@ -7,16 +7,19 @@ import (
 )
 
 type ValidatorState struct {
+	StateBase
 	PublicKey *crypto.PubKey
 }
 
 func(v *ValidatorState) Serialize(w io.Writer) error {
+	v.StateBase.Serialize(w)
 	v.PublicKey.Serialize(w)
 	return nil
 }
 
 
 func(v *ValidatorState)Deserialize(r io.Reader) error {
+	
 	p := new(crypto.PubKey)
 	p.DeSerialize(r)
 	v.PublicKey = p
@@ -24,7 +27,17 @@ func(v *ValidatorState)Deserialize(r io.Reader) error {
 }
 
 func(v *ValidatorState) ToArray() []byte {
-	b := new(bytes.Buffer)
-	v.Serialize(b)
-	return b.Bytes()
+	stateBase := new(StateBase)
+	err := stateBase.Deserialize(r)
+	if err != nil {
+		return err
+	}
+	v.StateBase = *stateBase
+	p := new(crypto.PubKey)
+	err = p.DeSerialize(r)
+	if err != nil {
+		return err
+	}
+	v.PublicKey = p
+	return nil
 }
