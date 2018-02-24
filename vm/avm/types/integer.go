8db3a11d1a8fc4fe7dc1277bda5cd3e63cdd5c63
oscar <oscar@nkn.org>
2018-02-24 21:41:38 +0800

    vm module migration (Phase I)
    Signed-off-by: oscar <oscar@nkn.org>

---
+++ vm/avm/types/integer.go
@@ -0,0 +1,50 @@
+package types
+
+import (
+	"math/big"
+	"nkn-core/vm/avm"
+	"nkn-core/vm/avm/interfaces"
+)
+
+type Integer struct {
+	value *big.Int
+}
+
+func NewInteger(value *big.Int) *Integer {
+	var i Integer
+	i.value = value
+	return &i
+}
+
+func (i *Integer) Equals(other StackItemInterface) bool {
+	if _, ok := other.(*Integer); !ok {
+		return false
+	}
+	if i.value.Cmp(other.GetBigInteger()) != 0 {
+		return false
+	}
+	return true
+}
+
+func (i *Integer) GetBigInteger() *big.Int {
+	return i.value
+}
+
+func (i *Integer) GetBoolean() bool {
+	if i.value.Cmp(big.NewInt(0)) == 0 {
+		return false
+	}
+	return true
+}
+
+func (i *Integer) GetByteArray() []byte {
+	return avm.ToArrayReverse(i.value.Bytes())
+}
+
+func (i *Integer) GetInterface() interfaces.IInteropInterface {
+	return nil
+}
+
+func (i *Integer) GetArray() []StackItemInterface {
+	return []StackItemInterface{i}
+}
---
+++ vm/avm/types/stackiteminterface.go
@@ -0,0 +1,15 @@
+package types
+
+import (
+	"math/big"
+	"nkn-core/vm/avm/interfaces"
+)
+
+type StackItemInterface interface {
+	Equals(other StackItemInterface) bool
+	GetBigInteger() *big.Int
+	GetBoolean() bool
+	GetByteArray() []byte
+	GetInterface() interfaces.IInteropInterface
+	GetArray() []StackItemInterface
+}
