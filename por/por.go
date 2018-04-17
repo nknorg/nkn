5521f22d6ffe468180648d0134162171316c2557
NoelBright <noel.n.bright@gmail.com>
2018-04-17 16:05:36 +0800

    Add method to interface IPor
    Add MergeRelays, IsRelaying, TotalRelays and IsTxProcessed to
    the interface IPor and structure RelayStat.
    
    Signed-off-by: NoelBright <noel.n.bright@gmail.com>

--- por/por.go
+++ por/por.go
@@ -9,4 +9,8 @@ type IPor interface {
 	CalcRelays(txn *transaction.Transaction) error
 	GetRelays(pk *crypto.PubKey) int
 	GetMaxRelay() crypto.PubKey
+	MergeRelays(por IPor) error
+	IsRelaying(pk *crypto.PubKey) bool
+	TotalRelays() int
+	IsTxProcessed(txn *transaction.Transaction) bool
 }
