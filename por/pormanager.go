b1923caaeed36acff1c62f99629fd21fa28ef7a2
NoelBright <noel.n.bright@gmail.com>
2018-04-27 15:57:00 +0800

    Add uniform struct porManager to manage POR module
    Other modules can use porManager to use functions of POR module.
    
    Signed-off-by: NoelBright <noel.n.bright@gmail.com>

---
+++ por/pormanager.go
@@ -0,0 +1,89 @@
+package por
+
+import (
+	"github.com/nknorg/nkn/crypto"
+	"github.com/nknorg/nkn/util/log"
+	"github.com/nknorg/nkn/wallet"
+)
+
+const (
+	PorChanCap = 5
+)
+
+//TODO
+//  Version() uint32
+//  Sign(sc *SigChain) error
+//  Verify(sc *SigChain) error
+//  IsFinal(sc *SigChain) bool
+//  GetSignture(sc *SigChain) ([]byte, error)
+//  GetLenofSig(sc *SigChain) uint32
+//  CommitSig() *transaction.Transaction
+//  GetSigFromTx(txn *transaction.Transaction) []byte
+//  GetSigChainFromTx(txn *transaction.Transaction) *SigChain
+type porManager struct {
+	account    *wallet.Account
+	maxWorkSig []byte
+	msgChan    chan interface{}
+	quit       chan struct{}
+	started    bool
+}
+
+func NewPorManager(acc *wallet.Account) *porManager {
+	pm := &porManager{
+		account: acc,
+		msgChan: make(chan interface{}, PorChanCap),
+		quit:    make(chan struct{}, 1),
+	}
+
+	go pm.porHandler()
+	pm.started = true
+	return pm
+}
+
+func (pm *porManager) porHandler() {
+out:
+	for {
+		select {
+		case m := <-pm.msgChan:
+			switch msg := m.(type) {
+			case *signMsg:
+				go pm.handleSignMsg(msg)
+			default:
+				log.Warnf("Invalid message type in block "+"handler: %T", msg)
+			}
+
+		case <-pm.quit:
+			break out
+		}
+	}
+
+	log.Trace("Por handler done")
+}
+
+type signMsg struct {
+	sc         *SigChain
+	nextPubkey *crypto.PubKey
+	reply      chan *SigChain
+}
+
+func (pm *porManager) handleSignMsg(sm *signMsg) {
+	if eq := crypto.Equal(pm.account.PubKey(), sm.sc.elems[len(sm.sc.elems)-1].pubkey); !eq {
+		sm.reply <- sm.sc
+	}
+	ne := NewElem(pm.account.PubKey(), sm.nextPubkey)
+	ne.Sign(sm.sc)
+	sm.sc.Sign(ne)
+	sm.reply <- sm.sc
+	//TODO if it's the destination address of sigchain, send commit
+	//transaction
+}
+func (pm *porManager) Sign(sc *SigChain, nextPubkey *crypto.PubKey) *SigChain {
+	if !pm.started {
+		return nil
+	}
+
+	rp := make(chan *SigChain, 1)
+	pm.msgChan <- &signMsg{sc: sc, nextPubkey: nextPubkey, reply: rp}
+	ret := <-rp
+	return ret
+}
