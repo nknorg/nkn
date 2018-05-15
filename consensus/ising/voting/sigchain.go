0e4f5af92059bffc579fa2c361c7ac0fbd62869f
oscar <oscar@nkn.org>
2018-05-15 18:34:59 +0800

    Implement Voting interface for SigChain
    Signed-off-by: oscar <oscar@nkn.org>

--- consensus/ising/voting/sigchain.go
+++ consensus/ising/voting/sigchain.go
@@ -4,39 +4,48 @@ import (
 	"sync"
 
 	. "github.com/nknorg/nkn/common"
+	"github.com/nknorg/nkn/core/ledger"
+	"github.com/nknorg/nkn/core/transaction"
+	"github.com/nknorg/nkn/por"
 )
 
 type SigChainVoting struct {
 	sync.RWMutex
 	pstate         map[Uint256]*State            // consensus state for proposer
 	vstate         map[uint64]map[Uint256]*State // consensus state for voter
-	confirmingHash Uint256                       // sigchain hash in process
+	porServer      *por.PorServer                // signature chain service provider
+	pool           *SigChainVotingPool           // signature chain voting pool
+	confirmingHash Uint256                       // signature chain hash in process
+	txnCollector   *transaction.TxnCollector     // transaction pool
 }
 
-func NewSigChainVoting() *SigChainVoting {
-	sigchainVoting := &SigChainVoting{
-		pstate: make(map[Uint256]*State),
-		vstate: make(map[uint64]map[Uint256]*State),
+func NewSigChainVoting(totalWeight int, porServer *por.PorServer, txnCollector *transaction.TxnCollector) *SigChainVoting {
+	sigChainVoting := &SigChainVoting{
+		pstate:       make(map[Uint256]*State),
+		vstate:       make(map[uint64]map[Uint256]*State),
+		porServer:    porServer,
+		pool:         NewSigChainVotingPool(totalWeight),
+		txnCollector: txnCollector,
 	}
 
-	return sigchainVoting
+	return sigChainVoting
 }
 
-func (scv *SigChainVoting) SetProposerState(blockhash Uint256, s State) {
+func (scv *SigChainVoting) SetProposerState(hash Uint256, s State) {
 	scv.Lock()
 	defer scv.Unlock()
 
-	if _, ok := scv.pstate[blockhash]; !ok {
-		scv.pstate[blockhash] = new(State)
+	if _, ok := scv.pstate[hash]; !ok {
+		scv.pstate[hash] = new(State)
 	}
-	scv.pstate[blockhash].SetBit(s)
+	scv.pstate[hash].SetBit(s)
 }
 
-func (scv *SigChainVoting) HasProposerState(blockhash Uint256, state State) bool {
+func (scv *SigChainVoting) HasProposerState(hash Uint256, state State) bool {
 	scv.RLock()
 	defer scv.RUnlock()
 
-	if v, ok := scv.pstate[blockhash]; !ok || v == nil {
+	if v, ok := scv.pstate[hash]; !ok || v == nil {
 		return false
 	} else {
 		if v.HasBit(state) {
@@ -46,21 +55,27 @@ func (scv *SigChainVoting) HasProposerState(blockhash Uint256, state State) bool
 	}
 }
 
-func (scv *SigChainVoting) SetVoterState(id uint64, blockhash Uint256, s State) {
+func (scv *SigChainVoting) SetVoterState(id uint64, hash Uint256, s State) {
+	scv.Lock()
+	defer scv.Unlock()
+
 	if _, ok := scv.vstate[id]; !ok {
 		scv.vstate[id] = make(map[Uint256]*State)
 	}
-	if _, ok := scv.vstate[id][blockhash]; !ok {
-		scv.vstate[id][blockhash] = new(State)
+	if _, ok := scv.vstate[id][hash]; !ok {
+		scv.vstate[id][hash] = new(State)
 	}
-	scv.vstate[id][blockhash].SetBit(s)
+	scv.vstate[id][hash].SetBit(s)
 }
 
-func (scv *SigChainVoting) HasVoterState(id uint64, blockhash Uint256, state State) bool {
+func (scv *SigChainVoting) HasVoterState(id uint64, hash Uint256, state State) bool {
+	scv.RLock()
+	defer scv.RUnlock()
+
 	if _, ok := scv.vstate[id]; !ok {
 		return false
 	} else {
-		if v, ok := scv.vstate[id][blockhash]; !ok || v == nil {
+		if v, ok := scv.vstate[id][hash]; !ok || v == nil {
 			return false
 		} else {
 			if v.HasBit(state) {
@@ -79,12 +94,34 @@ func (scv *SigChainVoting) GetConfirmingHash() Uint256 {
 	return scv.confirmingHash
 }
 
-func (scv *SigChainVoting) GetCurrentVotingContent() (VotingContent, error) {
+func (scv *SigChainVoting) GetBestVotingContent() (VotingContent, error) {
+	height := ledger.DefaultLedger.Store.GetHeight() + 1
+	txns := scv.txnCollector.Collect()
+	for _, txn := range txns {
+		if err := scv.porServer.AddSigChainFromTx(txn); err != nil {
+			return nil, err
+		}
+	}
+	sigChain, err := scv.porServer.GetMinSigChain(height)
+	if err != nil {
+		return nil, err
+	}
+
+	return sigChain, nil
+}
+
+func (scv *SigChainVoting) GetWorseVotingContent() (VotingContent, error) {
 	return nil, nil
 }
 
 func (scv *SigChainVoting) GetVotingContent(hash Uint256) (VotingContent, error) {
-	return nil, nil
+	height := ledger.DefaultLedger.Store.GetHeight() + 1
+	sigChain, err := scv.porServer.GetSigChain(height, hash)
+	if err != nil {
+		return nil, err
+	}
+
+	return sigChain, nil
 }
 
 func (scv *SigChainVoting) VotingType() VotingContentType {
@@ -96,7 +133,21 @@ func (scv *SigChainVoting) Preparing(content VotingContent) error {
 }
 
 func (scv *SigChainVoting) Exist(hash Uint256) bool {
-	return false
+	height := ledger.DefaultLedger.Store.GetHeight() + 1
+	txns := scv.txnCollector.Collect()
+	// TODO: skip duplicated signature chain
+	for _, txn := range txns {
+		if err := scv.porServer.AddSigChainFromTx(txn); err != nil {
+			return false
+		}
+	}
+	_, ret := scv.porServer.IsSigChainExist(hash, height)
+
+	return ret
+}
+
+func (scv *SigChainVoting) GetVotingPool() VotingPool {
+	return scv.pool
 }
 
 func (scv *SigChainVoting) DumpState(hash Uint256, desc string, verbose bool) {
---
+++ consensus/ising/voting/sigchainvotingpool.go
@@ -0,0 +1,142 @@
+package voting
+
+import (
+	"errors"
+	"sync"
+
+	. "github.com/nknorg/nkn/common"
+	"github.com/nknorg/nkn/core/ledger"
+	"github.com/nknorg/nkn/util/log"
+)
+
+type SigChainVotingPool struct {
+	sync.RWMutex
+	sendPool    map[uint64]map[uint32]Uint256 // record voting history
+	receivePool map[uint64]map[uint32]Uint256 // received votes from neighbor
+	mind        map[uint32]Uint256            // current idea
+	totalWeight int
+}
+
+func NewSigChainVotingPool(totalWeight int) *SigChainVotingPool {
+	return &SigChainVotingPool{
+		sendPool:    make(map[uint64]map[uint32]Uint256),
+		receivePool: make(map[uint64]map[uint32]Uint256),
+		mind:        make(map[uint32]Uint256),
+		totalWeight: totalWeight,
+	}
+}
+
+func (scvp *SigChainVotingPool) SetMind(height uint32, mind Uint256) {
+	scvp.Lock()
+	defer scvp.Unlock()
+
+	if hash, ok := scvp.mind[height]; !ok {
+		scvp.mind[height] = mind
+	} else {
+		if mind.CompareTo(hash) == -1 {
+			scvp.mind[height] = mind
+		}
+	}
+}
+
+func (scvp *SigChainVotingPool) GetMind(height uint32) Uint256 {
+	scvp.RLock()
+	defer scvp.RUnlock()
+
+	return scvp.mind[height]
+}
+
+func (scvp *SigChainVotingPool) ChangeMind(height uint32, mind Uint256) {
+	scvp.Lock()
+	defer scvp.Unlock()
+
+	scvp.mind[height] = mind
+	// TODO: broadcast
+}
+
+func (scvp *SigChainVotingPool) AddToReceivePool(nid uint64, height uint32, hash Uint256) {
+	scvp.Lock()
+	defer scvp.Unlock()
+
+	if _, ok := scvp.receivePool[nid]; !ok {
+		scvp.receivePool[nid] = make(map[uint32]Uint256)
+	}
+	if _, ok := scvp.receivePool[nid][height]; !ok {
+		scvp.receivePool[nid][height] = hash
+	}
+}
+
+func (scvp *SigChainVotingPool) AddToSendPool(nid uint64, height uint32, hash Uint256) {
+	scvp.Lock()
+	defer scvp.Unlock()
+
+	if _, ok := scvp.sendPool[nid]; !ok {
+		scvp.sendPool[nid] = make(map[uint32]Uint256)
+	}
+	if _, ok := scvp.sendPool[nid][height]; !ok {
+		scvp.sendPool[nid][height] = hash
+	}
+}
+
+func (scvp *SigChainVotingPool) VoteCounting(height uint32) (*Uint256, error) {
+	scvp.RLock()
+	defer scvp.RUnlock()
+
+	m := make(map[Uint256]int)
+	// counting votes from neighbors
+	for _, item := range scvp.receivePool {
+		for h, hash := range item {
+			if h == height {
+				weight, err := ledger.DefaultLedger.Store.GetVotingWeight(Uint160{})
+				if err != nil {
+					log.Warn("get voter weight error")
+					return nil, errors.New("get voter weight error")
+				}
+				if _, ok := m[hash]; ok {
+					m[hash] += weight
+				} else {
+					m[hash] = weight
+				}
+			}
+		}
+	}
+	// self vote
+	if selfMind, ok := scvp.mind[height]; !ok {
+		return nil, errors.New("self mind missing")
+	} else {
+		selfWeight, err := ledger.DefaultLedger.Store.GetVotingWeight(Uint160{})
+		if err != nil {
+			log.Warn("get self weight error")
+			return nil, errors.New("get self weight error")
+		}
+		if _, ok := m[selfMind]; ok {
+			m[selfMind] += selfWeight
+		} else {
+			m[selfMind] = selfWeight
+		}
+	}
+	// returns hash which got >=50% votes
+	for hash, count := range m {
+		if 2*count >= scvp.totalWeight {
+			return &hash, nil
+		}
+	}
+
+	return nil, errors.New("invalid votes")
+}
+
+func (scvp *SigChainVotingPool) NeedChangeMind(height uint32, hash Uint256) bool {
+	if hash.CompareTo(scvp.GetMind(height)) == 0 {
+		return false
+	}
+
+	return true
+}
+
+func (scvp *SigChainVotingPool) SpreadNewMind() {
+	//TODO: notify neighbors in sendpool
+}
+
+func (scvp *SigChainVotingPool) Reset(totalWeight int) {
+	scvp.totalWeight = totalWeight
+}
