02c2b3db4a499239aeff4c44e4143904424775ac
oscar <oscar@nkn.org>
2018-05-29 18:04:20 +0800

    VotingPool optimizing
    1. use height as main key and vote counting optimizing
    2. return another bool for GetMind to indicate mind has been set or not.
    3. remove unused methods
    
    Signed-off-by: oscar <oscar@nkn.org>

--- consensus/ising/voting/blockvotingpool.go
+++ consensus/ising/voting/blockvotingpool.go
@@ -11,70 +11,73 @@ import (
 
 type BlockVotingPool struct {
 	sync.RWMutex
-	sendPool    map[uint64]map[uint32]Uint256 // record voting history
-	receivePool map[uint64]map[uint32]Uint256 // received votes from neighbor
+	sendPool    map[uint32]map[uint64]Uint256 // record voting history
+	receivePool map[uint32]map[uint64]Uint256 // received votes from neighbor
 	mind        map[uint32]Uint256            // current idea
 	totalWeight int
 }
 
 func NewBlockVotingPool(totalWeight int) *BlockVotingPool {
 	return &BlockVotingPool{
-		sendPool:    make(map[uint64]map[uint32]Uint256),
-		receivePool: make(map[uint64]map[uint32]Uint256),
+		sendPool:    make(map[uint32]map[uint64]Uint256),
+		receivePool: make(map[uint32]map[uint64]Uint256),
 		mind:        make(map[uint32]Uint256),
 		totalWeight: totalWeight,
 	}
 }
 
-func (bvp *BlockVotingPool) SetMind(height uint32, mind Uint256) {
-	bvp.Lock()
-	defer bvp.Unlock()
-
-	if hash, ok := bvp.mind[height]; !ok {
-		bvp.mind[height] = mind
-	} else {
-		if mind.CompareTo(hash) == -1 {
-			bvp.mind[height] = mind
-		}
-	}
-}
-
-func (bvp *BlockVotingPool) GetMind(height uint32) Uint256 {
+func (bvp *BlockVotingPool) GetMind(height uint32) (Uint256, bool) {
 	bvp.RLock()
 	defer bvp.RUnlock()
 
-	return bvp.mind[height]
+	if mind, ok := bvp.mind[height]; ok {
+		return mind, true
+	} else {
+		return Uint256{}, false
+	}
 }
 
-func (bvp *BlockVotingPool) ChangeMind(height uint32, mind Uint256) {
+func (bvp *BlockVotingPool) ChangeMind(height uint32, mind Uint256) map[uint64]Uint256 {
 	bvp.Lock()
-	defer bvp.Unlock()
-
 	bvp.mind[height] = mind
-	// TODO: broadcast
+	bvp.Unlock()
+
+	// add self vote to pool
+	bvp.AddToReceivePool(height, 0, mind)
+
+	bvp.RLock()
+	defer bvp.RUnlock()
+
+	return bvp.sendPool[height]
 }
 
-func (bvp *BlockVotingPool) AddToReceivePool(nid uint64, height uint32, hash Uint256) {
+func (bvp *BlockVotingPool) AddToReceivePool(height uint32, nid uint64, hash Uint256) {
 	bvp.Lock()
 	defer bvp.Unlock()
 
-	if _, ok := bvp.receivePool[nid]; !ok {
-		bvp.receivePool[nid] = make(map[uint32]Uint256)
+	if _, ok := bvp.receivePool[height]; !ok {
+		bvp.receivePool[height] = make(map[uint64]Uint256)
 	}
-	if _, ok := bvp.receivePool[nid][height]; !ok {
-		bvp.receivePool[nid][height] = hash
-	}
+
+	bvp.receivePool[height][nid] = hash
 }
 
-func (bvp *BlockVotingPool) AddToSendPool(nid uint64, height uint32, hash Uint256) {
+func (bvp *BlockVotingPool) GetReceivePool(height uint32) map[uint64]Uint256 {
+	bvp.RLock()
+	defer bvp.RUnlock()
+
+	return bvp.receivePool[height]
+}
+
+func (bvp *BlockVotingPool) AddToSendPool(height uint32, nid uint64, hash Uint256) {
 	bvp.Lock()
 	defer bvp.Unlock()
 
-	if _, ok := bvp.sendPool[nid]; !ok {
-		bvp.sendPool[nid] = make(map[uint32]Uint256)
+	if _, ok := bvp.sendPool[height]; !ok {
+		bvp.sendPool[height] = make(map[uint64]Uint256)
 	}
-	if _, ok := bvp.sendPool[nid][height]; !ok {
-		bvp.sendPool[nid][height] = hash
+	if _, ok := bvp.sendPool[height][nid]; !ok {
+		bvp.sendPool[height][nid] = hash
 	}
 }
 
@@ -83,36 +86,18 @@ func (bvp *BlockVotingPool) VoteCounting(height uint32) (*Uint256, error) {
 	defer bvp.RUnlock()
 
 	m := make(map[Uint256]int)
-	// counting votes from neighbors
-	for _, item := range bvp.receivePool {
-		for h, hash := range item {
-			if h == height {
-				weight, err := ledger.DefaultLedger.Store.GetVotingWeight(Uint160{})
-				if err != nil {
-					log.Warn("get voter weight error")
-					return nil, errors.New("get voter weight error")
-				}
-				if _, ok := m[hash]; ok {
-					m[hash] += weight
-				} else {
-					m[hash] = weight
-				}
-			}
-		}
-	}
-	// self vote
-	if selfMind, ok := bvp.mind[height]; !ok {
-		return nil, errors.New("self mind missing")
-	} else {
-		selfWeight, err := ledger.DefaultLedger.Store.GetVotingWeight(Uint160{})
+	// vote counting
+	for _, hash := range bvp.receivePool[height] {
+		weight, err := ledger.DefaultLedger.Store.GetVotingWeight(Uint160{})
 		if err != nil {
-			log.Warn("get self weight error")
-			return nil, errors.New("get self weight error")
+			log.Warn("get voter weight error")
+			return nil, errors.New("get voter weight error")
 		}
-		if _, ok := m[selfMind]; ok {
-			m[selfMind] += selfWeight
+
+		if _, ok := m[hash]; ok {
+			m[hash] += weight
 		} else {
-			m[selfMind] = selfWeight
+			m[hash] = weight
 		}
 	}
 	// returns hash which got >=50% votes
@@ -123,18 +108,6 @@ func (bvp *BlockVotingPool) VoteCounting(height uint32) (*Uint256, error) {
 	}
 
 	return nil, errors.New("invalid votes")
-}
-
-func (bvp *BlockVotingPool) NeedChangeMind(height uint32, hash Uint256) bool {
-	if hash.CompareTo(bvp.GetMind(height)) == 0 {
-		return false
-	}
-
-	return true
-}
-
-func (bvp *BlockVotingPool) SpreadNewMind() {
-	//TODO: notify neighbors in sendpool
 }
 
 func (bvp *BlockVotingPool) Reset(totalWeight int) {
