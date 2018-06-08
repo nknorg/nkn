89c020d811842d46c3fc8797e81cf4e7393ee24e
gdmmx <gdmmx@163.com>
2018-06-08 21:01:16 +0800

    [Feature] Add one-button launch script for Cloud deploy

---
+++ test/launch_node.sh
@@ -0,0 +1,34 @@
+#! /bin/bash
+
+function Usage () {
+    local RED="\E[1;31m"
+    local GREEN="\E[1;32m"
+    local YELLOW="\E[1;33m"
+    local BLUE="\E[1;34m"
+    local END="\E[0m"
+    printf "${RED}Usage${END}: ${BLUE}%s${END} ${GREEN}<EXEC_DIR>${END} ${YELLOW}[join|create]${END}\n" $1
+    printf "${RED}Example${END}: ${BLUE}%s${END} ${GREEN}./testbed/node0001${END} ${YELLOW}join${END}\n" $1
+}
+
+function initWallet () {
+    rm -f wallet.dat
+    ./nknc wallet -c <<EOF
+testbed
+testbed
+EOF
+    return $?
+}
+
+function start () {
+    ./nknd -test $1 <<EOF
+testbed
+EOF
+}
+
+[ -n "$1" ] || ! Usage $0 || exit $EINVAL
+EXEC_DIR=$1
+[ -n "$2" ] && MODE=$2 || MODE="join"
+
+cd ${EXEC_DIR}
+[ -e "wallet.dat" ] || initWallet || ! echo "Init Wallet fail" || exit 1
+start ${MODE}
