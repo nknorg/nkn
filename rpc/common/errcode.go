7a2f86d2c479558b63c0197838a18b1fa0a2586e
NoelBright <noel.n.bright@gmail.com>
2018-06-20 18:46:48 +0800

    [Phase 4/5] Add uniform interfaces for jsonrpc, restful and websocket
    Plan:
    phase 1: move 1/3 interfaces of httpjson/ to common/ and something else.
    phase 2: move 1/3 interfaces of httpjson/ to common/.
    phase 3: move 1/3 interfaces of httpjson/ to common/.
    phase 4: add comments to interfaces in common/ and modify return code to
    	 errorcode correspondingly.
    phase 5: remove processUnify from httpjson and embedded interfaces directly.
    
    [rpc/common][rpc/jsonrpc]
    Add an adapter 'processUnify' to be compatible with old interfaces in
    jsonrpc temporarily. In the future the 'processUnify' will be remove.
    
    1. add comments to interfaces in common/
    2. modify return code to errorcode correspondingly.
    
    Signed-off-by: NoelBright <noel.n.bright@gmail.com>

--- rpc/common/errcode.go
+++ rpc/common/errcode.go
@@ -1,61 +1,58 @@
 package common
 
+import "github.com/nknorg/nkn/errors"
+
 type ErrCode int64
 
 const (
-	SUCCESS            ErrCode = 0
-	SESSION_EXPIRED    ErrCode = 41001
-	SERVICE_CEILING    ErrCode = 41002
-	ILLEGAL_DATAFORMAT ErrCode = 41003
-
-	INVALID_METHOD ErrCode = 42001
-	INVALID_PARAMS ErrCode = 42002
-	INVALID_TOKEN  ErrCode = 42003
-
+	SUCCESS             ErrCode = 0
+	SESSION_EXPIRED     ErrCode = 41001
+	SERVICE_CEILING     ErrCode = 41002
+	ILLEGAL_DATAFORMAT  ErrCode = 41003
+	INVALID_METHOD      ErrCode = 42001
+	INVALID_PARAMS      ErrCode = 42002
+	INVALID_TOKEN       ErrCode = 42003
 	INVALID_TRANSACTION ErrCode = 43001
 	INVALID_ASSET       ErrCode = 43002
 	INVALID_BLOCK       ErrCode = 43003
-
+	INVALID_HASH        ErrCode = 43004
+	INVALID_VERSION     ErrCode = 43005
 	UNKNOWN_TRANSACTION ErrCode = 44001
 	UNKNOWN_ASSET       ErrCode = 44002
 	UNKNOWN_BLOCK       ErrCode = 44003
-
-	INVALID_VERSION ErrCode = 45001
-	INTERNAL_ERROR  ErrCode = 45002
-
-	SMARTCODE_ERROR ErrCode = 47001
+	UNKNOWN_HASH        ErrCode = 44004
+	INTERNAL_ERROR      ErrCode = 45001
+	SMARTCODE_ERROR     ErrCode = 47001
 )
 
-var ErrMap = map[ErrCode]string{
-	SUCCESS:            "SUCCESS",
-	SESSION_EXPIRED:    "SESSION EXPIRED",
-	SERVICE_CEILING:    "SERVICE CEILING",
-	ILLEGAL_DATAFORMAT: "ILLEGAL DATAFORMAT",
-
-	INVALID_METHOD: "INVALID METHOD",
-	INVALID_PARAMS: "INVALID PARAMS",
-	INVALID_TOKEN:  "VERIFY TOKEN ERROR",
-
-	INVALID_TRANSACTION: "INVALID TRANSACTION",
-	INVALID_ASSET:       "INVALID ASSET",
-	INVALID_BLOCK:       "INVALID BLOCK",
-
-	UNKNOWN_TRANSACTION: "UNKNOWN TRANSACTION",
-	UNKNOWN_ASSET:       "UNKNOWN ASSET",
-	UNKNOWN_BLOCK:       "UNKNOWN BLOCK",
-
-	INVALID_VERSION: "INVALID VERSION",
-	INTERNAL_ERROR:  "INTERNAL ERROR",
-	SMARTCODE_ERROR: "SMARTCODE EXEC ERROR",
-	//ErrCode(ErrDuplicateInput):       "INTERNAL ERROR, ErrDuplicateInput",
-	//ErrCode(ErrAssetPrecision):       "INTERNAL ERROR, ErrAssetPrecision",
-	//ErrCode(ErrTransactionBalance):   "INTERNAL ERROR, ErrTransactionBalance",
-	//ErrCode(ErrAttributeProgram):     "INTERNAL ERROR, ErrAttributeProgram",
-	//ErrCode(ErrTransactionContracts): "INTERNAL ERROR, ErrTransactionContracts",
-	//ErrCode(ErrTransactionPayload):   "INTERNAL ERROR, ErrTransactionPayload",
-	//ErrCode(ErrDoubleSpend):          "INTERNAL ERROR, ErrDoubleSpend",
-	//ErrCode(ErrTxHashDuplicate):      "INTERNAL ERROR, ErrTxHashDuplicate",
-	//ErrCode(ErrStateUpdaterVaild):    "INTERNAL ERROR, ErrStateUpdaterVaild",
-	//ErrCode(ErrSummaryAsset):         "INTERNAL ERROR, ErrSummaryAsset",
-	//ErrCode(ErrXmitFail):             "INTERNAL ERROR, ErrXmitFail",
+var ErrMessage = map[ErrCode]string{
+	SUCCESS:                                 "SUCCESS",
+	SESSION_EXPIRED:                         "SESSION EXPIRED",
+	SERVICE_CEILING:                         "SERVICE CEILING",
+	ILLEGAL_DATAFORMAT:                      "ILLEGAL DATAFORMAT",
+	INVALID_METHOD:                          "INVALID METHOD",
+	INVALID_PARAMS:                          "INVALID PARAMS",
+	INVALID_TOKEN:                           "VERIFY TOKEN ERROR",
+	INVALID_TRANSACTION:                     "INVALID TRANSACTION",
+	INVALID_ASSET:                           "INVALID ASSET",
+	INVALID_BLOCK:                           "INVALID BLOCK",
+	INVALID_HASH:                            "INVALID HASH",
+	UNKNOWN_TRANSACTION:                     "UNKNOWN TRANSACTION",
+	UNKNOWN_ASSET:                           "UNKNOWN ASSET",
+	UNKNOWN_BLOCK:                           "UNKNOWN BLOCK",
+	UNKNOWN_HASH:                            "UNKNOWN BLOCK",
+	INVALID_VERSION:                         "INVALID VERSION",
+	INTERNAL_ERROR:                          "INTERNAL ERROR",
+	SMARTCODE_ERROR:                         "SMARTCODE EXEC ERROR",
+	ErrCode(errors.ErrDuplicateInput):       "INTERNAL ERROR, ErrDuplicateInput",
+	ErrCode(errors.ErrAssetPrecision):       "INTERNAL ERROR, ErrAssetPrecision",
+	ErrCode(errors.ErrTransactionBalance):   "INTERNAL ERROR, ErrTransactionBalance",
+	ErrCode(errors.ErrAttributeProgram):     "INTERNAL ERROR, ErrAttributeProgram",
+	ErrCode(errors.ErrTransactionContracts): "INTERNAL ERROR, ErrTransactionContracts",
+	ErrCode(errors.ErrTransactionPayload):   "INTERNAL ERROR, ErrTransactionPayload",
+	ErrCode(errors.ErrDoubleSpend):          "INTERNAL ERROR, ErrDoubleSpend",
+	ErrCode(errors.ErrTxHashDuplicate):      "INTERNAL ERROR, ErrTxHashDuplicate",
+	ErrCode(errors.ErrStateUpdaterVaild):    "INTERNAL ERROR, ErrStateUpdaterVaild",
+	ErrCode(errors.ErrSummaryAsset):         "INTERNAL ERROR, ErrSummaryAsset",
+	ErrCode(errors.ErrXmitFail):             "INTERNAL ERROR, ErrXmitFail",
 }
