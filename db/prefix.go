a4dcddbd6afe03440859b4ca69279a251830ba1e
NoelBright <noel.n.bright@gmail.com>
2018-09-04 16:00:14 +0800

    add separate get key function for each prefix.
    contain: assetKey ,blockhashKey ,bookkeeperKey ,currentBlockHashKey ,headerKey ,issuseQuantKey ,iteratorAsset ,iteratorBlockHash ,iteratorUTXO ,paddingKey ,prepaidKey ,transactionKey ,unspentIndexKey ,unspentUtxoKey ,versionKey.
    
    Signed-off-by: NoelBright <noel.n.bright@gmail.com>

--- db/prefix.go
+++ db/prefix.go
@@ -1,5 +1,11 @@
 package db
 
+import (
+	"encoding/binary"
+
+	"github.com/nknorg/nkn/common"
+)
+
 type DataEntryPrefix byte
 
 const (
@@ -28,3 +34,71 @@ const (
 	//CONFIG
 	CFG_Version DataEntryPrefix = 0xf0
 )
+
+func paddingKey(prefix DataEntryPrefix, key []byte) []byte {
+	return append([]byte{byte(prefix)}, key...)
+}
+
+func versionKey() []byte {
+	return paddingKey(CFG_Version, nil)
+}
+
+func currentBlockHashKey() []byte {
+	return paddingKey(SYS_CurrentBlock, nil)
+}
+
+func bookkeeperKey() []byte {
+	return paddingKey(SYS_CurrentBookKeeper, nil)
+}
+
+func assetKey(assetId common.Uint256) []byte {
+	return paddingKey(ST_Info, assetId.ToArray())
+}
+
+func issuseQuantKey(assetId common.Uint256) []byte {
+	return paddingKey(ST_QuantityIssued, assetId.ToArray())
+}
+
+func prepaidKey(programHash common.Uint160) []byte {
+	return paddingKey(ST_Prepaid, programHash.ToArray())
+}
+
+func unspentIndexKey(txHash common.Uint256) []byte {
+	return paddingKey(IX_Unspent, txHash.ToArray())
+}
+
+func unspentUtxoKey(programHash common.Uint160, assetid common.Uint256, height uint32) []byte {
+	heightBuffer := make([]byte, 4)
+	binary.LittleEndian.PutUint32(heightBuffer[:], height)
+	key := append(append(programHash.ToArray(), assetid.ToArray()...), heightBuffer...)
+
+	return paddingKey(IX_Unspent_UTXO, key)
+}
+
+func blockhashKey(height uint32) []byte {
+	heightBuffer := make([]byte, 4)
+	binary.LittleEndian.PutUint32(heightBuffer[:], height)
+
+	return paddingKey(DATA_BlockHash, heightBuffer)
+}
+
+func headerKey(blockHash common.Uint256) []byte {
+	return paddingKey(DATA_Header, blockHash.ToArray())
+}
+
+func transactionKey(txHash common.Uint256) []byte {
+	return paddingKey(DATA_Transaction, txHash.ToArray())
+}
+
+func iteratorBlockHash(store IStore) IIterator {
+	return store.NewIterator([]byte{byte(DATA_BlockHash)})
+}
+
+func iteratorAsset(store IStore) IIterator {
+	return store.NewIterator([]byte{byte(ST_Info)})
+}
+
+func iteratorUTXO(store IStore, key []byte) IIterator {
+	return store.NewIterator(append([]byte{byte(IX_Unspent_UTXO)}, key...))
+
+}
