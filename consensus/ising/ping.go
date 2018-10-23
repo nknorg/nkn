11700fb36abd1cf11d6df903be36af5e8d944c92
Yilun <zyl.skysniper@gmail.com>
2018-10-22 19:52:09 -0700

    Get neighbor SyncState in consensus ping/pong msg
    Signed-off-by: Yilun <zyl.skysniper@gmail.com>

--- consensus/ising/ping.go
+++ consensus/ising/ping.go
@@ -4,20 +4,28 @@ import (
 	"io"
 
 	"github.com/nknorg/nkn/common/serialization"
+	"github.com/nknorg/nkn/net/protocol"
 )
 
 type Ping struct {
-	height uint32 // which height want to ping
+	height    uint32 // which height want to ping
+	syncState protocol.SyncState
 }
 
-func NewPing(height uint32) *Ping {
+func NewPing(height uint32, syncState protocol.SyncState) *Ping {
 	return &Ping{
-		height: height,
+		height:    height,
+		syncState: syncState,
 	}
 }
 
 func (p *Ping) Serialize(w io.Writer) error {
 	err := serialization.WriteUint32(w, p.height)
+	if err != nil {
+		return err
+	}
+
+	err = serialization.WriteByte(w, byte(p.syncState))
 	if err != nil {
 		return err
 	}
@@ -31,6 +39,12 @@ func (p *Ping) Deserialize(r io.Reader) error {
 		return err
 	}
 	p.height = height
+
+	state, err := serialization.ReadByte(r)
+	if err != nil {
+		return err
+	}
+	p.syncState = protocol.SyncState(state)
 
 	return nil
 }
