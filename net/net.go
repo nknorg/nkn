2ccadfcfd1527856838fef26a5b0cdfe7b186805
Yilun <zyl.skysniper@gmail.com>
2018-11-02 03:10:37 -0700

    Stop accepting inbound node before chord is ready
    Signed-off-by: Yilun <zyl.skysniper@gmail.com>

--- net/net.go
+++ net/net.go
@@ -8,15 +8,5 @@ import (
 )
 
 func StartProtocol(pubKey *crypto.PubKey, nn *nnet.NNet) (protocol.Noder, error) {
-	net, err := node.InitNode(pubKey, nn)
-	if err != nil {
-		return nil, err
-	}
-
-	err = nn.Start()
-	if err != nil {
-		return nil, err
-	}
-
-	return net, nil
+	return node.InitNode(pubKey, nn)
 }
