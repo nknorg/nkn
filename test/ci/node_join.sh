f5b87ebde72a00aa667e79b5873933980b917d4e
gdmmx <gdmmx@163.com>
2018-12-20 18:58:37 +0800

    Auto-update to latest tag instead of latest commit
    Signed-off-by: gdmmx <gdmmx@163.com>

--- test/ci/node_join.sh
+++ test/ci/node_join.sh
@@ -9,7 +9,7 @@ NKN_HOME=$(realpath $CI_DIR/../..)
 BIN_DIR="testbed/node_0001"
 
 ### Setup seed list by cmdline or hard code a default
-[ -z "$1" ] && DEFAULT_SEED="35.201.21.23:30003" || DEFAULT_SEED=$1
+[ -z "$1" ] && DEFAULT_SEED="" || DEFAULT_SEED=$1
 
 ###
 # func: init bin dir and adjust config.json for adapted to run on cloud
@@ -19,7 +19,7 @@ BIN_DIR="testbed/node_0001"
 function init_bin () {
     ./test/create_testbed.sh 2 $(dirname ${1})
     sed -i '/127.0.0.1/d' ./${1}/config.json
-    sed -i '/SeedList/a"'$2'"' ./${1}/config.json
+    [ -n "$2" ] && sed -i '/SeedList/a"'$2'",' ./${1}/config.json
 }
 
 ### init bin dir if it not exist
@@ -27,8 +27,10 @@ cd $NKN_HOME
 [ -d ./${BIN_DIR} ] || init_bin ${BIN_DIR} ${DEFAULT_SEED}
 
 ### Obtain latest src code and build binary
-git pull
+git fetch
+LATEST_TAG=$(git for-each-ref --sort=-taggerdate --count=1 --format "%(refname:short)" refs/tags)
+git checkout ${LATEST_TAG}
 make
 cp -a nkn[cd] ./${BIN_DIR}/
 
-./test/launch_node.sh ./${BIN_DIR}/ --no-check-port
+./test/launch_node.sh ./${BIN_DIR}/ --no-nat
