98108fc7446bc7521292fdbc1c21b98998677b2c
Yilun <zyl.skysniper@gmail.com>
2018-12-22 03:18:27 -0800

    Change /dev/random back to /dev/urandom and ulimit in test script
    Signed-off-by: Yilun <zyl.skysniper@gmail.com>

--- test/create_testbed.sh
+++ test/create_testbed.sh
@@ -32,7 +32,7 @@ do
 
     ### init wallet.dat
     rm -f wallet.dat
-    RANDOM_PASSWD=$(head -c 64 /dev/random |base64 |head -c 32)
+    RANDOM_PASSWD=$(head -c 1024 /dev/urandom | shasum -a 512 -b | xxd -r -p | base64 | head -c 32)
     ./nknc wallet -c <<EOF
 ${RANDOM_PASSWD}
 ${RANDOM_PASSWD}
