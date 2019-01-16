eb2b230b861d6cb248509f4081eb42c80fd4d5a8
Yilun <zyl.skysniper@gmail.com>
2019-01-16 05:30:53 -0800

    Add bitShiftPerSigChainElement to prefer longer signature chain to be selected
    Signed-off-by: Yilun <zyl.skysniper@gmail.com>

---
+++ por/util.go
@@ -0,0 +1,13 @@
+package por
+
+func rightShiftBytes(b []byte, n int) {
+	for k := 0; k < n; k++ {
+		b[len(b)-1] >>= 1
+		for i := len(b) - 2; i >= 0; i-- {
+			if b[i]&0x1 == 0x1 {
+				b[i+1] |= 0x80
+			}
+			b[i] >>= 1
+		}
+	}
+}
