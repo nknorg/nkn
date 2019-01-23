421a168c91eef13d624274a55338167b0ec35729
NoelBright <noel.n.bright@gmail.com>
2019-01-23 17:14:04 +0800

    Optimize trie and others
    1. optimize trie and others
    
    Signed-off-by: NoelBright <noel.n.bright@gmail.com>

--- trie/node.go
+++ trie/node.go
@@ -9,7 +9,7 @@ import (
 	"github.com/nknorg/nkn/common/serialization"
 )
 
-var indices = []string{"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f", "17"}
+var indices = []string{"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f", "[17]"}
 
 type node interface {
 	fString(string) string
