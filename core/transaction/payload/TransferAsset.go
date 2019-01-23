518c54b33736ea52a21a61961c1b857e71ffcb61
NoelBright <noel.n.bright@gmail.com>
2019-01-23 16:24:48 +0800

    Modify transaction module to adapter balance
    Signed-off-by: NoelBright <noel.n.bright@gmail.com>

--- core/transaction/payload/TransferAsset.go
+++ core/transaction/payload/TransferAsset.go
@@ -1,31 +1,82 @@
 package payload
 
-import "io"
+import (
+	"encoding/json"
+	"io"
 
-const TransferAssetayloadVersion byte = 0x00
+	"github.com/nknorg/nkn/common"
+)
 
 type TransferAsset struct {
+	Sender    common.Uint160
+	Recipient common.Uint160
+	Amount    common.Fixed64
 }
 
 func (a *TransferAsset) Data(version byte) []byte {
-	//TODO: implement TransferAsset.Data()
 	return []byte{0}
 
 }
 
 func (a *TransferAsset) Serialize(w io.Writer, version byte) error {
+	a.Sender.Serialize(w)
+	a.Recipient.Serialize(w)
+	a.Amount.Serialize(w)
+
 	return nil
 }
 
 func (a *TransferAsset) Deserialize(r io.Reader, version byte) error {
+	a.Sender.Deserialize(r)
+	a.Recipient.Deserialize(r)
+	a.Amount.Deserialize(r)
 	return nil
 }
 
 func (a *TransferAsset) MarshalJson() ([]byte, error) {
-	return []byte{}, nil
+	cb := &TransferAssetInfo{
+		Sender:    common.BytesToHexString(a.Sender.ToArray()),
+		Recipient: common.BytesToHexString(a.Recipient.ToArray()),
+		Amount:    a.Amount.String(),
+	}
+
+	data, err := json.Marshal(cb)
+	if err != nil {
+		return nil, err
+	}
+	return data, nil
 }
 
 func (a *TransferAsset) UnmarshalJson(data []byte) error {
-	a = &TransferAsset{}
+	cb := new(TransferAssetInfo)
+
+	var err error
+	if err = json.Unmarshal(data, &cb); err != nil {
+		return err
+	}
+
+	sender, err := common.HexStringToBytes(cb.Sender)
+	if err != nil {
+		return err
+	}
+	a.Sender, err = common.Uint160ParseFromBytes(sender)
+	if err != nil {
+		return err
+	}
+
+	recipient, err := common.HexStringToBytes(cb.Recipient)
+	if err != nil {
+		return err
+	}
+	a.Recipient, err = common.Uint160ParseFromBytes(recipient)
+	if err != nil {
+		return err
+	}
+
+	a.Amount, err = common.StringToFixed64(cb.Amount)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
--- core/transaction/payload/jsontype.go
+++ core/transaction/payload/jsontype.go
@@ -1,29 +1,5 @@
 package payload
 
-import (
-	"github.com/nknorg/nkn/core/asset"
-	"github.com/nknorg/nkn/crypto"
-)
-
-// define RegisterAssetInfo
-type RegisterAssetInfo struct {
-	Asset      *asset.Asset   `json:"asset"`
-	Amount     string         `json:"amount"`
-	Issuer     *crypto.PubKey `json:"issuer"`
-	Controller string         `json:"controller"`
-}
-
-// define PrepaidInfo
-type PrepaidInfo struct {
-	Asset  string `json:"asset"`
-	Amount string `json:"amount"`
-	Rates  string `json:"rates"`
-}
-
-type WithdrawInfo struct {
-	ProgramHash string `json:"programHash"`
-}
-
 type CommitInfo struct {
 	SigChain  string `json:"sigChain"`
 	Submitter string `json:"submitter"`
@@ -46,4 +22,16 @@ type SubscribeInfo struct {
 	Bucket     uint32 `json:"bucket"`
 	Duration   uint32 `json:"duration"`
 	Meta       string `json:"meta"`
+}
+
+type TransferAssetInfo struct {
+	Sender    string `json:"sender"`
+	Recipient string `json:"recipient"`
+	Amount    string `json:"amount"`
+}
+
+type CoinbaseInfo struct {
+	Sender    string `json:"sender"`
+	Recipient string `json:"recipient"`
+	Amount    string `json:"amount"`
 }
