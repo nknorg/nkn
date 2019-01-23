bf8e0d2af80de495777b53a5088c682f7f91967b
NoelBright <noel.n.bright@gmail.com>
2019-01-23 16:25:39 +0800

    modify ledger to adapter balance
    1. modify Block & Header structure
    2. modify blockchain flow
    
    Signed-off-by: NoelBright <noel.n.bright@gmail.com>

--- core/ledger/ledgerStore.go
+++ core/ledger/ledgerStore.go
@@ -2,20 +2,19 @@ package ledger
 
 import (
 	. "github.com/nknorg/nkn/common"
-	. "github.com/nknorg/nkn/core/asset"
 	tx "github.com/nknorg/nkn/core/transaction"
 )
 
 // ILedgerStore provides func with store package.
 type ILedgerStore interface {
-	SaveBlock(b *Block, ledger *Ledger) error
+	SaveBlock(b *Block, ledger *Ledger, fastAdd bool) error
 	GetBlock(hash Uint256) (*Block, error)
 	GetBlockByHeight(height uint32) (*Block, error)
-	BlockInCache(hash Uint256) bool
+	//BlockInCache(hash Uint256) bool
 	GetBlockHash(height uint32) (Uint256, error)
-	GetBlockHistory(startHeight, blockNum uint32) map[uint32]Uint256
-	CheckBlockHistory(history map[uint32]Uint256) (uint32, bool)
-	GetVotingWeight(hash Uint160) (int, error)
+	//GetBlockHistory(startHeight, blockNum uint32) map[uint32]Uint256
+	//CheckBlockHistory(history map[uint32]Uint256) (uint32, bool)
+	//GetVotingWeight(hash Uint160) (int, error)
 
 	IsDoubleSpend(tx *tx.Transaction) bool
 
@@ -25,8 +24,8 @@ type ILedgerStore interface {
 
 	GetTransaction(hash Uint256) (*tx.Transaction, error)
 
-	SaveAsset(assetid Uint256, asset *Asset) error
-	GetAsset(hash Uint256) (*Asset, error)
+	//SaveAsset(assetid Uint256, asset *Asset) error
+	//GetAsset(hash Uint256) (*Asset, error)
 
 	SaveName(registrant []byte, name string) error
 	GetName(registrant []byte) (*string, error)
@@ -38,8 +37,11 @@ type ILedgerStore interface {
 	GetFirstAvailableTopicBucket(topic string) int
 	GetTopicBucketsCount(topic string) uint32
 
-	GetContract(codeHash Uint160) ([]byte, error)
-	GetStorage(key []byte) ([]byte, error)
+	GetStateRootHash() Uint256
+	GetBalance(addr Uint160) Fixed64
+
+	//GetContract(codeHash Uint160) ([]byte, error)
+	//GetStorage(key []byte) ([]byte, error)
 
 	GetCurrentBlockHash() Uint256
 	GetCurrentHeaderHash() Uint256
@@ -50,15 +52,14 @@ type ILedgerStore interface {
 
 	InitLedgerStoreWithGenesisBlock(genesisblock *Block) (uint32, error)
 
-	GetQuantityIssued(assetid Uint256) (Fixed64, error)
+	//GetQuantityIssued(assetid Uint256) (Fixed64, error)
+	//GetUnspent(txid Uint256, index uint16) (*tx.TxnOutput, error)
+	//ContainsUnspent(txid Uint256, index uint16) (bool, error)
+	//GetUnspentFromProgramHash(programHash Uint160, assetid Uint256) ([]*tx.UTXOUnspent, error)
+	//GetUnspentsFromProgramHash(programHash Uint160) (map[Uint256][]*tx.UTXOUnspent, error)
+	//GetPrepaidInfo(programHash Uint160) (*Fixed64, *Fixed64, error)
 
-	GetUnspent(txid Uint256, index uint16) (*tx.TxnOutput, error)
-	ContainsUnspent(txid Uint256, index uint16) (bool, error)
-	GetUnspentFromProgramHash(programHash Uint160, assetid Uint256) ([]*tx.UTXOUnspent, error)
-	GetUnspentsFromProgramHash(programHash Uint160) (map[Uint256][]*tx.UTXOUnspent, error)
-	GetPrepaidInfo(programHash Uint160) (*Fixed64, *Fixed64, error)
-
-	GetAssets() map[Uint256]*Asset
+	//GetAssets() map[Uint256]*Asset
 
 	IsTxHashDuplicate(txhash Uint256) bool
 	IsBlockInStore(hash Uint256) bool
