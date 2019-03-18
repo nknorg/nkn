1eff6d2986e29e16cbf2356dbe31c8309d191d58
NoelBright <noel.n.bright@gmail.com>
2019-03-18 18:07:29 +0800

    optimize API module
    1. Replace MarshalJson function with GetInfo for Transaction, Header and Block.
    2. complete GetAllTransactions() of TxnPool.
    
    Signed-off-by: NoelBright <noel.n.bright@gmail.com>

--- api/common/interfaces.go
+++ api/common/interfaces.go
@@ -99,10 +99,9 @@ func getBlock(s Serverer, params map[string]interface{}) map[string]interface{} 
 	if err != nil {
 		return respPacking(nil, UNKNOWN_BLOCK)
 	}
-	block.Hash()
 
 	var b interface{}
-	info, _ := block.MarshalJson()
+	info, _ := block.GetInfo()
 	json.Unmarshal(info, &b)
 
 	return respPacking(b, SUCCESS)
@@ -229,7 +228,7 @@ func getRawMemPool(s Serverer, params map[string]interface{}) map[string]interfa
 	txs := []interface{}{}
 	txpool := localNode.GetTxnPool()
 	for _, t := range txpool.GetAllTransactions() {
-		info, err := t.MarshalJson()
+		info, err := t.GetInfo()
 		if err != nil {
 			return respPacking(nil, INTERNAL_ERROR)
 		}
@@ -272,7 +271,7 @@ func getTransaction(s Serverer, params map[string]interface{}) map[string]interf
 
 		tx.Hash()
 		var tran interface{}
-		info, _ := tx.MarshalJson()
+		info, _ := tx.GetInfo()
 		json.Unmarshal(info, &tran)
 
 		return respPacking(tran, SUCCESS)
