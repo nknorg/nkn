2aa7d10dc056be22d5aea13a97f66de90b5912ee
NoelBright <noel.n.bright@gmail.com>
2019-04-11 16:09:05 +0800

    modify mining incentive
    Mining incentive contains two components. One is mining reward that is
    written in whitepaper. The other is the donation that be send to a special
    address to reward miner.
    
    Signed-off-by: NoelBright <noel.n.bright@gmail.com>

---
+++ chain/reward.go
@@ -0,0 +1,30 @@
+package chain
+
+import (
+	"github.com/nknorg/nkn/common"
+	"github.com/nknorg/nkn/util/config"
+)
+
+var (
+	MiningRewards = make(map[uint32]common.Fixed64)
+)
+
+func init() {
+	for i := uint32(0); i < config.TotalRewardDuration; i++ {
+		MiningRewards[i] = common.Fixed64(config.InitialReward - config.ReductionAmount*int(i))
+	}
+}
+
+func GetRewardByHeight(height uint32) common.Fixed64 {
+	if height == 0 {
+		panic("invalid height")
+	}
+	duration := (height - 1) / uint32(config.RewardAdjustInterval)
+	if duration >= config.TotalRewardDuration {
+		return 0
+	}
+
+	reward := MiningRewards[duration]
+	rewardPerBlock := int64(reward) / int64(config.RewardAdjustInterval)
+	return common.Fixed64(rewardPerBlock)
+}
