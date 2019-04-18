ede01ce38eae63d1b31c93fe4bc89c4e2ec0d139
gdmmx <gdmmx@163.com>
2019-04-18 11:49:33 +0800

    Add pubkey2addr utility
    Signed-off-by: gdmmx <gdmmx@163.com>

---
+++ util/pubkey2addr.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"encoding/hex"
+	"fmt"
+	"os"
+
+	"github.com/nknorg/nkn/common"
+	"github.com/nknorg/nkn/crypto"
+	"github.com/nknorg/nkn/vm/contract"
+)
+
+func Key2Address(key string) (addr string, err error) {
+	var k []byte
+	var pk *crypto.PubKey
+	var redeemHash common.Uint160
+
+	if k, err = hex.DecodeString(key); err == nil {
+		if pk, err = crypto.DecodePoint(k); err == nil {
+			if redeemHash, err = contract.CreateRedeemHash(pk); err == nil {
+				return redeemHash.ToAddress()
+			}
+		}
+	}
+	return "", err
+}
+
+func main() {
+	crypto.SetAlg("Ed25519")
+	for _, key := range os.Args[1:] {
+		addr, err := Key2Address(key)
+		if err != nil {
+			os.Stderr.WriteString(err.Error() + "\n")
+			continue
+		}
+		fmt.Println(addr)
+	}
+}
