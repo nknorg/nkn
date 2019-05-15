1b2a8851c5d863caa848b007bbc78cf650a3f510
NoelBright <noel.n.bright@gmail.com>
2019-05-15 11:16:01 +0800

    add processing flow with fee
    add fee to coinase reward and reduce from sender.
    
    Signed-off-by: NoelBright <noel.n.bright@gmail.com>

--- chain/stateGenerator.go
+++ chain/stateGenerator.go
@@ -8,7 +8,7 @@ import (
 	"github.com/nknorg/nnet/log"
 )
 
-func spendTransaction(states *db.StateDB, tx *Transaction, genesis bool) error {
+func spendTransaction(states *db.StateDB, tx *Transaction, totalFee common.Fixed64, genesis bool) error {
 	pl, err := Unpack(tx.UnsignedTx.Payload)
 	if err != nil {
 		log.Error("unpack payload error", err)
@@ -34,14 +34,14 @@ func spendTransaction(states *db.StateDB, tx *Transaction, genesis bool) error {
 
 		acc := states.GetOrNewAccount(common.BytesToUint160(coinbase.Recipient))
 		amount := acc.GetBalance()
-		acc.SetBalance(amount + common.Fixed64(coinbase.Amount))
+		acc.SetBalance(amount + common.Fixed64(coinbase.Amount) + totalFee)
 		states.SetAccount(common.BytesToUint160(coinbase.Recipient), acc)
 
 	case TransferAssetType:
 		transfer := pl.(*TransferAsset)
 		accSender := states.GetOrNewAccount(common.BytesToUint160(transfer.Sender))
 		amountSender := accSender.GetBalance()
-		accSender.SetBalance(amountSender - common.Fixed64(transfer.Amount))
+		accSender.SetBalance(amountSender - common.Fixed64(transfer.Amount) - common.Fixed64(tx.UnsignedTx.Fee))
 		nonceSender := accSender.GetNonce()
 		accSender.SetNonce(nonceSender + 1)
 		states.SetAccount(common.BytesToUint160(transfer.Sender), accSender)
@@ -57,6 +57,8 @@ func spendTransaction(states *db.StateDB, tx *Transaction, genesis bool) error {
 	case SubscribeType:
 		pg, _ := common.ToCodeHash(tx.Programs[0].Code)
 		accSender := states.GetOrNewAccount(pg)
+		amountSender := accSender.GetBalance()
+		accSender.SetBalance(amountSender - common.Fixed64(tx.UnsignedTx.Fee))
 		nonceSender := accSender.GetNonce()
 		accSender.SetNonce(nonceSender + 1)
 		states.SetAccount(pg, accSender)
@@ -70,8 +72,13 @@ func GenerateStateRoot(txs []*Transaction) common.Uint256 {
 	root := DefaultLedger.Store.GetCurrentBlockStateRoot()
 	states, _ := db.NewStateDB(root, db.NewTrieStore(DefaultLedger.Store.GetDatabase()))
 
+	var totalFee common.Fixed64
 	for _, tx := range txs {
-		spendTransaction(states, tx, false)
+		totalFee += common.Fixed64(tx.UnsignedTx.Fee)
+	}
+
+	for _, tx := range txs {
+		spendTransaction(states, tx, totalFee, false)
 	}
 
 	return states.IntermediateRoot(true)
@@ -82,7 +89,7 @@ func GenesisStateRoot(store ILedgerStore, txs []*Transaction) common.Uint256 {
 	states, _ := db.NewStateDB(root, db.NewTrieStore(store.GetDatabase()))
 
 	for _, tx := range txs {
-		spendTransaction(states, tx, true)
+		spendTransaction(states, tx, 0, true)
 	}
 
 	return states.IntermediateRoot(true)
