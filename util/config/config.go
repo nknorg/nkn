24a1b492fe2dd61ba38756aba4bb74da668c6596
gdmmx <gdmmx@nkn.org>
2019-05-21 10:33:55 +0000

    Upgrade TestNet/Protocol/DB version for divide up previous incompatible v09 testnet
    Signed-off-by: gdmmx <gdmmx@nkn.org>

--- util/config/config.go
+++ util/config/config.go
@@ -32,7 +32,7 @@ const (
 	MaxRollbackBlocks            = 1
 	SigChainPropogationTime      = 1
 	EncryptAlg                   = "Ed25519"
-	DBVersion                    = 0x04
+	DBVersion                    = 0x05
 	InitialIssueAddress          = "NKNQ83xc8zQNEE6WBDKm7tZrLwoMwAq4c4jo"
 	InitialIssueAmount           = 700000000 * common.StorageFactor
 	TotalMiningRewards           = 300000000 * common.StorageFactor
