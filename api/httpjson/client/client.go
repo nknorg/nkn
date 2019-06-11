da2c60574c48f5348e2070de8ed98a5c925be860
NoelBright <noel.n.bright@gmail.com>
2019-06-11 17:42:14 +0800

    use GenerateID transaction to generate chordid
    1. genesisChoidID = zerotxhash + zeroblockhash + block(0).randBeacon
    2. when a node try to join nkn network:
    2.1 look up local ledger to get chord id; if not found,
    2.2 request chord id from remote seed node; if not foud,
    2.3 create a chord id by sending a GenerateID transaction.
    2.4 request chord id from remote node periodically.
    2.4 if time out, quit nknd.
    
    Signed-off-by: NoelBright <noel.n.bright@gmail.com>

--- api/httpjson/client/client.go
+++ api/httpjson/client/client.go
@@ -9,6 +9,10 @@ import (
 	"net/http"
 	"os"
 	"strings"
+
+	"github.com/nknorg/nkn/api/common"
+	. "github.com/nknorg/nkn/common"
+	"github.com/nknorg/nkn/crypto"
 )
 
 // Call sends RPC request to server
@@ -76,4 +80,134 @@ func FindSuccessorAddrs(remote string, key []byte) ([]string, error) {
 	log.Printf("Successor Address: %v\n", ret)
 
 	return ret.Result, nil
+}
+
+func GetMyExtIP(remote string, ip []byte) (string, error) {
+	resp, err := Call(remote, "getmyextip", 0, map[string]interface{}{
+		"RemoteAddr": ip,
+	})
+	if err != nil {
+		return "", err
+	}
+	log.Printf("GetMyExtIP got resp: %v from %s\n", string(resp), remote)
+
+	var ret struct {
+		Result struct{ RemoteAddr string } `json:"result"`
+		Err    map[string]interface{}      `json:"error"`
+	}
+	if err := json.Unmarshal(resp, &ret); err != nil {
+		log.Println(err)
+		return "", err
+	}
+	if len(ret.Err) != 0 { // resp.error NOT empty
+		return "", fmt.Errorf("GetMyExtIP(%s) resp error: %v", remote, string(resp))
+	}
+	log.Printf("From %s got myself ExtIP: %s\n", remote, string(resp))
+
+	return ret.Result.RemoteAddr, nil
+}
+
+func GetID(remote string, publicKey []byte) ([]byte, error) {
+	resp, err := Call(remote, "getid", 0, map[string]interface{}{
+		"publickey": BytesToHexString(publicKey),
+	})
+	if err != nil {
+		return nil, err
+	}
+	log.Printf("GetID got resp: %v from %s\n", string(resp), remote)
+
+	var ret struct {
+		Result struct{ Id string }    `json:"result"`
+		Err    map[string]interface{} `json:"error"`
+	}
+	if err := json.Unmarshal(resp, &ret); err != nil {
+		log.Println(err)
+		return nil, err
+	}
+	if len(ret.Err) != 0 { // resp.error NOT empty
+		code, ok := ret.Err["code"].(float64)
+		if !ok {
+			return nil, fmt.Errorf("GetID resp error,interface conversion faild")
+		}
+		if int64(code) == -int64(common.ErrZeroID) {
+			return crypto.Sha256ZeroHash, nil
+		}
+
+		return nil, fmt.Errorf("GetID(%s) resp error: %v", remote, string(resp))
+	}
+
+	idSlice, err := HexStringToBytes(ret.Result.Id)
+	if err != nil {
+		return nil, err
+	}
+
+	return idSlice, nil
+}
+
+func CreateID(remote string, genIdTxn string) (string, error) {
+	params := map[string]interface{}{
+		"tx": genIdTxn,
+	}
+
+	resp, err := Call(remote, "sendrawtransaction", 0, params)
+	if err != nil {
+		return "", err
+	}
+
+	log.Printf("CreateID got resp: %v from %s\n", string(resp), remote)
+
+	var ret struct {
+		Result string                 `json:"result"`
+		Err    map[string]interface{} `json:"error"`
+	}
+
+	if err := json.Unmarshal(resp, &ret); err != nil {
+		log.Println(err)
+		return "", err
+	}
+	if len(ret.Err) != 0 { // resp.error NOT empty
+		code, ok := ret.Err["code"].(float64)
+		if !ok {
+			return "", fmt.Errorf("CreateID resp error,interface conversion faild")
+		}
+
+		if int64(code) == -int64(common.ErrDuplicatedTx) {
+			return "", nil
+		}
+		return "", fmt.Errorf("CreateID(%s) resp error: %v", remote, string(resp))
+	}
+
+	return ret.Result, nil
+}
+
+func GetNonceByAddr(remote string, addr string) (uint64, error) {
+	params := map[string]interface{}{
+		"address": addr,
+	}
+
+	resp, err := Call(remote, "getnoncebyaddr", 0, params)
+	if err != nil {
+		return 0, err
+	}
+
+	log.Printf("GetNonceByAddr got resp: %v from %s\n", string(resp), remote)
+
+	var ret struct {
+		Result struct {
+			nonce         uint64
+			nonceInTxPool uint64
+			currentHeight uint32
+		} `json:"result"`
+		Err map[string]interface{} `json:"error"`
+	}
+
+	if err := json.Unmarshal(resp, &ret); err != nil {
+		log.Println(err)
+		return 0, err
+	}
+	if len(ret.Err) != 0 { // resp.error NOT empty
+		return 0, fmt.Errorf("GetNonceByAddr(%s) resp error: %v", remote, string(resp))
+	}
+
+	return ret.Result.nonceInTxPool, nil
 }
