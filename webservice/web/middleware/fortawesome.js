a0f5286d38b96d7662fde25e1011367b17d0d178
heron <peng.liu@nkn.org>
2019-06-24 13:05:15 +0800

    1. add web service files 2. add config WebServicePort default 30000 3. edit log. web log file is xxx_WEBLOG.log

---
+++ webservice/web/middleware/fortawesome.js
@@ -0,0 +1,16 @@
+import '@fortawesome/fontawesome-free/css/all.css'
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+
+Vue.component('font-awesome-icon', FontAwesomeIcon) // Register component globally
+library.add(fas) // Include needed icons.
+
+// Vue.use(Vuetify, {
+//   iconfont: 'faSvg',
+// })
+Vue.use(Vuetify, {
+  iconfont: 'fa'
+})
