1362a157310e968cd58b02b262a8d687b3423861
heron <peng.liu@nkn.org>
2019-06-26 15:09:27 +0800

    add dashboard node status file

--- dashboard/web/components/status/NodeRunStatus.vue
+++ dashboard/web/components/status/NodeRunStatus.vue
@@ -1,15 +1,51 @@
 <template>
   <v-badge color="transparent" class="breathe mr-4">
     <template v-slot:badge>
-      <v-icon dark small color="green accent-4">lens</v-icon>
+      <v-icon dark small :color="getStatusColor(nodeStatus.syncState)">lens</v-icon>
     </template>
-    <span>Node status: starting</span>
+    <span>{{$t('node_status.NODE_STATUS')}}: {{getStatus( nodeStatus.syncState)}}</span>
   </v-badge>
 </template>
 
 <script>
   export default {
-    name: "NodeStatus"
+    name: "NodeStatus",
+    props: {
+      nodeStatus: {
+        type: Object,
+        required: true
+      }
+    },
+    data: () => ({
+      color: {
+        'DEFAULT': 'grey',
+        'WAIT_FOR_SYNCING': 'yellow accent-2',
+        'SYNC_STARTED': 'green accent-4',
+        'SYNC_FINISHED': 'green accent-4',
+        'PERSIST_FINISHED': 'green accent-4'
+      }
+    }),
+    computed: {},
+    mounted() {
+
+    },
+    methods: {
+      // get node status
+      getStatus(stateStr) {
+        const statusEnum = {
+          'DEFAULT': this.$t('node.state.DEFAULT'),
+          'WAIT_FOR_SYNCING': this.$t('node.state.WAIT_FOR_SYNCING'),
+          'SYNC_STARTED': this.$t('node.state.SYNC_STARTED'),
+          'SYNC_FINISHED': this.$t('node.state.SYNC_FINISHED'),
+          'PERSIST_FINISHED': this.$t('node.state.PERSIST_FINISHED')
+        }
+        return statusEnum[stateStr] || this.$t('node.state.DEFAULT')
+      },
+      getStatusColor(stateStr) {
+        stateStr = stateStr || 'DEFAULT'
+        return this.color[stateStr]
+      }
+    }
   }
 </script>
 
---
+++ dashboard/web/locales/zh/node.js
@@ -0,0 +1,10 @@
+export default {
+  state:{
+    DEFAULT: '等待运行',
+    WAIT_FOR_SYNCING: '等待账本同步',
+    SYNC_STARTED: '账本开始同步',
+    SYNC_FINISHED: '账本同步完成',
+    PERSIST_FINISHED: '运行中'
+  }
+
+}
