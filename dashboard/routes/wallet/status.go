a118b00a97ccb936efd7739e148323e91ebe3bd8
heron <peng.liu@nkn.org>
2019-06-26 12:25:31 +0800

    edit service node status feature

--- dashboard/routes/wallet/status.go
+++ dashboard/routes/wallet/status.go
@@ -1,7 +1,12 @@
 package wallet
 
 import (
+	"encoding/hex"
 	"github.com/gin-gonic/gin"
+	"github.com/nknorg/nkn/chain"
+	"github.com/nknorg/nkn/common"
+	"github.com/nknorg/nkn/util/log"
+	"github.com/nknorg/nkn/vault"
 	"net/http"
 )
 
@@ -9,15 +14,38 @@ type WalletRouter struct {
 }
 
 func (walletRouter *WalletRouter) Router(router *gin.RouterGroup) {
-	router.GET("/wallet/status", func(context *gin.Context) {
+	router.GET("/current-wallet/status", func(context *gin.Context) {
+		var err error
 		wallet, exists := context.Get("wallet")
+
+		account, err := wallet.(vault.Wallet).GetDefaultAccount()
+		if err != nil {
+			log.WebLog.Error("get wallet account error: ", err)
+			context.AbortWithError(http.StatusInternalServerError, err)
+		}
+
+		address, err := account.ProgramHash.ToAddress()
+		if err != nil {
+			log.WebLog.Error("get wallet address error: ", err)
+			context.AbortWithError(http.StatusInternalServerError, err)
+		}
+
+		pg, err := common.ToScriptHash(address)
+		if err != nil {
+			log.WebLog.Error("get wallet address error: ", err)
+			context.AbortWithError(http.StatusInternalServerError, err)
+		}
+
+		balance := chain.DefaultLedger.Store.GetBalance(pg)
+
 		if exists {
-			context.JSON(http.StatusOK, wallet)
+			context.JSON(http.StatusOK, gin.H{
+				"balance":   balance.String(),
+				"address":   address,
+				"publicKey": hex.EncodeToString(account.PublicKey.EncodePoint()),
+			})
 			return
 		}
 
-		context.JSON(http.StatusOK, gin.H{
-			"status": "...",
-		})
 	})
 }
