681f3af5ea4295e3a0d37f63eb423821cf102234
heron <peng.liu@nkn.org>
2019-06-26 15:09:27 +0800

    add dashboard node status file

--- dashboard/web/locales/en/index.js
+++ dashboard/web/locales/en/index.js
@@ -1,5 +1,24 @@
 import Menu from './menu'
+import Node from './node'
+
 export default {
   language: 'English',
-  menu: Menu
+  menu: Menu,
+  node: Node,
+
+  node_status: {
+    TITLE: 'Node status',
+    NODE_STATUS: 'Node status',
+    NODE_VERSION: 'Node version',
+    RELAY_MESSAGE_COUNT: 'Message relayed by node',
+    HEIGHT: 'NKN network block height',
+    BENEFICIARY_ADDR: 'Beneficiary address'
+  },
+  current_wallet_status: {
+    TITLE: 'Current wallet status',
+    BALANCE: 'Balance',
+    WALLET_ADDRESS: 'Wallet address',
+    PUBLIC_KEY: 'Public key',
+    PRIVATE_KEY: 'Private key'
+  }
 }
