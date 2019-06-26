41b875ac6c39c558cb9984b06d92ad472d7e2ce0
heron <peng.liu@nkn.org>
2019-06-26 15:09:27 +0800

    add dashboard node status file

---
+++ dashboard/web/locales/en/node.js
@@ -0,0 +1,10 @@
+export default {
+  state: {
+    DEFAULT: 'wait for running.',
+    WAIT_FOR_SYNCING: 'wait for chain db syncing.',
+    SYNC_STARTED: 'chain db sync started.',
+    SYNC_FINISHED: 'chain db sync finished.',
+    PERSIST_FINISHED: 'started.'
+  }
+
+}
