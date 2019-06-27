94f57e11146dc6b94d0f4bf9749e3596d0e6ece8
heron <peng.liu@nkn.org>
2019-06-27 16:56:56 +0800

    add dashboard node/current wallet status

--- dashboard/auth/auth.go
+++ dashboard/auth/auth.go
@@ -1,21 +1,57 @@
 package auth
 
 import (
+	"bytes"
+	"crypto/sha256"
 	"errors"
 	"github.com/gin-gonic/gin"
-	"log"
+	. "github.com/nknorg/nkn/common"
+	"github.com/nknorg/nkn/util/log"
+	"github.com/nknorg/nkn/vault"
+	"net/http"
 )
 
-func Auth() gin.HandlerFunc {
+func verifyPasswordKey(passwordKey []byte, passwordHash []byte) bool {
+	keyHash := sha256.Sum256(passwordKey)
+	if !bytes.Equal(passwordHash, keyHash[:]) {
+		return false
+	}
+
+	return true
+}
+
+// request filter: header["Authorization"] = (passwordhash)
+func WalletAuth() gin.HandlerFunc {
 	return func(context *gin.Context) {
-		log.Println("--------------------")
 		auth := context.GetHeader("Authorization")
-		log.Print(auth)
+
 		if auth == "" {
-			//context.JSON(403, gin.H{"message":"ooss"})
-			context.AbortWithError(403, errors.New("auth error"))
+			context.AbortWithError(http.StatusUnauthorized, errors.New("401 Unauthorized"))
 			return
 		}
+		wallet, exists := context.Get("wallet")
+		if !exists {
+			context.AbortWithError(http.StatusInternalServerError, errors.New("wallet is not init"))
+			return
+		}
+
+		passwordKey, err := HexStringToBytes(auth)
+		if err != nil {
+			log.WebLog.Error(err)
+			context.AbortWithError(http.StatusForbidden, err)
+			return
+		}
+		passwordKeyHash, err := HexStringToBytes(wallet.(*vault.WalletImpl).Data.PasswordHash)
+		if err != nil {
+			log.WebLog.Error(err)
+			context.AbortWithError(http.StatusInternalServerError, err)
+			return
+		}
+		if ok := verifyPasswordKey(passwordKey, passwordKeyHash[:]); !ok {
+			context.AbortWithError(http.StatusForbidden, errors.New("403 Forbidden"))
+			return
+		}
+
 		context.Next()
 	}
 }
