b363a07b4229b20b694506329b8290b50b3647f9
heron <peng.liu@nkn.org>
2019-06-28 18:55:12 +0800

    add web ui settings

--- dashboard/web/locales/en/settings.js
+++ dashboard/web/locales/en/settings.js
@@ -3,6 +3,7 @@ export default {
   RESTART_NKN: 'Restart NKN.',
   BENEFICIARY_TITLE: 'Input a beneficiary address for auto transfer.',
   BENEFICIARY_HINT: 'New amount will auto transfer to this address.',
-
-
+  BENEFICIARY_REQUIRED: 'Beneficiary is required',
+  BENEFICIARY_ERROR: 'Invalid Beneficiary.',
+  BENEFICIARY_WARING: 'Need restart NKN for enabled auto transfer feature. (New beneficiary address: {beneficiaryAddr})'
 }
