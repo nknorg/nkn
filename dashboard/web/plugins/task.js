f725b08dfd0c2aec5b6876c3372cb933cbbb5b96
heron <peng.liu@nkn.org>
2019-06-28 11:11:23 +0800

    move loop task to task plugins

---
+++ dashboard/web/plugins/task.js
@@ -0,0 +1,12 @@
+import {startLoopTask} from '~/helpers/task'
+
+export default function ({store}) {
+  // loop task
+  startLoopTask(function () {
+    store.dispatch('node/getNodeStatus')
+  }, 1000)
+  startLoopTask(function () {
+    store.dispatch('wallet/getCurrentWalletStatus')
+  }, 1000)
+
+}
