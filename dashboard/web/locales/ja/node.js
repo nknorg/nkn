312231d7a56bff7ab3a9596cb92535ffc8028cfd
heron <peng.liu@nkn.org>
2019-06-30 18:10:15 +0800

    feature neighbor web ui
    Signed-off-by: heron <peng.liu@nkn.org>

---
+++ dashboard/web/locales/ja/node.js
@@ -0,0 +1,10 @@
+export default {
+  state:{
+    DEFAULT: '実行待ち',
+    WAIT_FOR_SYNCING: 'チェーンが待っています',
+    SYNC_STARTED: 'チェーンが始まった',
+    SYNC_FINISHED: 'チェーンが終わった',
+    PERSIST_FINISHED: '起動'
+  }
+
+}
