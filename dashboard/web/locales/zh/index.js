e6d883571a99712f4c3eec88d6af97b6060a2aa5
heron <peng.liu@nkn.org>
2019-07-10 13:43:53 +0800

    add signin ui
    Signed-off-by: heron <peng.liu@nkn.org>

--- dashboard/web/locales/zh/index.js
+++ dashboard/web/locales/zh/index.js
@@ -28,9 +28,17 @@ export default {
   PASSWORD_REQUIRED: '密码不能为空.',
   PASSWORD_HINT: '请输入钱包密码.',
 
+  PASSWORD_CONFIRM: '确认密码',
+  PASSWORD_CONFIRM_ERROR: '两次密码不一致.',
+  PASSWORD_CONFIRM_REQUIRED: '确认密码不能为空.',
+  PASSWORD_CONFIRM_HINT: '请再次输入密码.',
+
+  WALLET_CREATE: '创建钱包',
+  CREATE: '创建',
+
   NO_DATA: '没有数据',
 
-  footer:{
+  footer: {
     TITLE: 'NKN：新一代互联网的网络基础设施',
     TEXT: 'NKN是区块链技术驱动的一种开放、去中心化的新型网络。NKN倡导用户共享网络资源，鼓励大家构建人人为我，我为人人的共建共享对等网络，在让共建者因协助数据传输而获得经济回报的同时为开发者提供一个开放，便捷，高效和安全的网络平台，让所有人都能体验更好的网络应用和服务。'
   },
