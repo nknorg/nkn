ddf96c6e049ad4d8582b7bf8f9f61c953075e5d3
heron <peng.liu@nkn.org>
2019-07-11 20:42:03 +0800

    update language neighbor field
    Signed-off-by: heron <peng.liu@nkn.org>

--- dashboard/web/locales/zh/neighbor.js
+++ dashboard/web/locales/zh/neighbor.js
@@ -1,4 +1,15 @@
 export default {
   TITLE: '邻居',
-  WARNING: '警告：没有入向邻居。或许外界访问不进来。'
+  WARNING: '警告：没有入向邻居。或许外界访问不进来。',
+
+  OUT_BOUND: '出站',
+  IN_BOUND: '进站',
+
+  header:{
+    ID: 'ID',
+    IP: 'IP',
+    STATE: '状态',
+    BOUND: '连接方向',
+    PING: 'Ping'
+  }
 }
