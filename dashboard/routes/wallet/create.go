0f2ed7160a8fcc182e0d032d436a63b5fab07339
heron <peng.liu@nkn.org>
2019-07-11 17:11:08 +0800

    web service add open/create wallet
    Signed-off-by: heron <peng.liu@nkn.org>

---
+++ dashboard/routes/wallet/create.go
@@ -0,0 +1,50 @@
+package wallet
+
+import (
+	"encoding/json"
+	"errors"
+	"github.com/gin-gonic/gin"
+	"github.com/nknorg/nkn/dashboard/helpers"
+	"github.com/nknorg/nkn/util/config"
+	"github.com/nknorg/nkn/util/log"
+	"github.com/nknorg/nkn/util/password"
+	"github.com/nknorg/nkn/vault"
+	"net/http"
+)
+
+type CreateWalletData struct {
+	Password string `form:"password" binding:"required"`
+}
+
+func WalletCreateRouter(router *gin.RouterGroup) {
+	router.POST("/wallet/create", func(context *gin.Context) {
+		bodyData := helpers.DecryptData(context)
+
+		var data CreateWalletData
+		err := json.Unmarshal([]byte(bodyData), &data)
+		if err != nil {
+			log.WebLog.Error(err)
+			context.AbortWithError(http.StatusBadRequest, err)
+			return
+		}
+
+		_, exists := context.Get("wallet")
+		if exists {
+			log.WebLog.Error("wallet has exist.")
+			context.AbortWithError(http.StatusInternalServerError, errors.New("wallet has exist."))
+			return
+		} else {
+			_, err := vault.NewWallet(config.Parameters.WalletFile, []byte(data.Password), true)
+			if err != nil {
+				log.WebLog.Error("create wallet error: ", err)
+				context.AbortWithError(http.StatusInternalServerError, err)
+				return
+			}
+			password.Passwd = data.Password
+			context.JSON(http.StatusOK, "")
+			return
+		}
+
+	})
+
+}
