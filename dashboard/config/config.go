af57b1cbc9d6c8aeeddfc62fe7cbe30ba4935ffc
heron <peng.liu@nkn.org>
2019-07-11 17:11:08 +0800

    web service add open/create wallet
    Signed-off-by: heron <peng.liu@nkn.org>

--- dashboard/config/config.go
+++ dashboard/config/config.go
@@ -4,7 +4,15 @@ const (
 	UnixRange = 10
 )
 
+const (
+	SERVICE_STATUS_DEFAULT        = 0
+	SERVICE_STATUS_NO_WALLET_FILE = 1
+	SERVICE_STATUS_NO_PASSWORD    = 2
+	SERVICE_STATUS_RUNNING        = 3
+)
+
 var (
 	IsInit   = false
 	IsRemote = false
+	Status   = SERVICE_STATUS_DEFAULT
 )
