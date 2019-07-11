62bfea3d1bb47260940cf932d94d57dacf8e635b
heron <peng.liu@nkn.org>
2019-07-11 20:42:03 +0800

    update language neighbor field
    Signed-off-by: heron <peng.liu@nkn.org>

--- dashboard/web/components/widget/Neighbors.vue
+++ dashboard/web/components/widget/Neighbors.vue
@@ -1,49 +1,49 @@
 <template>
-  <v-card min-width="800">
-    <v-card-title class="headline">{{$t('neighbor.TITLE')}}</v-card-title>
-    <div class="divider"></div>
-    <v-card-text>
-      <v-layout wrap>
-        <v-flex xs12 sm6 offset-sm6>
-          <v-text-field
-            v-model="search"
-            append-icon="search"
-            :label="$t('SEARCH')"
-            single-line
-            hide-details
-          ></v-text-field>
-        </v-flex>
-        <v-flex>
-          <v-data-table
-            :headers="headers"
-            :items="neighbors"
-            :search="search"
-            prev-icon="chevron_left"
-            next-icon="chevron_right"
-            sort-icon="arrow_upward"
-            :no-data-text="$t('NO_DATA')"
-            :pagination.sync="pagination"
-            rows-per-page-text=""
-            :rows-per-page-items="[0]"
-          >
-            <template slot="headerCell" slot-scope="{ header }">
-              <span class="subheading font-weight-light" v-text="header.text"/>
-            </template>
-            <template v-slot:items="props">
-              <td>{{ props.item.id }}</td>
-              <td>{{ props.item.addr }}</td>
-              <td>{{ getStatus(props.item.syncState) }}</td>
-              <td>{{ props.item.isOutbound }}</td>
-              <td>{{ props.item.roundTripTime }}</td>
-            </template>
-            <template v-slot:pageText="props">
-              {{ props.pageStart }} - {{ props.pageStop }} of {{ props.itemsLength }}
-            </template>
-          </v-data-table>
-        </v-flex>
-      </v-layout>
-    </v-card-text>
-  </v-card>
+    <v-card min-width="800">
+        <v-card-title class="headline">{{$t('neighbor.TITLE')}}</v-card-title>
+        <div class="divider"></div>
+        <v-card-text>
+            <v-layout wrap>
+                <v-flex xs12 sm6 offset-sm6>
+                    <v-text-field
+                            v-model="search"
+                            append-icon="search"
+                            :label="$t('SEARCH')"
+                            single-line
+                            hide-details
+                    ></v-text-field>
+                </v-flex>
+                <v-flex>
+                    <v-data-table
+                            :headers="headers"
+                            :items="neighbors"
+                            :search="search"
+                            prev-icon="chevron_left"
+                            next-icon="chevron_right"
+                            sort-icon="arrow_upward"
+                            :no-data-text="$t('NO_DATA')"
+                            :pagination.sync="pagination"
+                            rows-per-page-text=""
+                            :rows-per-page-items="[0]"
+                    >
+                        <template slot="headerCell" slot-scope="{ header }">
+                            <span class="subheading font-weight-light" v-text="header.text"/>
+                        </template>
+                        <template v-slot:items="props">
+                            <td>{{ props.item.id }}</td>
+                            <td>{{ props.item.addr }}</td>
+                            <td>{{ getStatus(props.item.syncState) }}</td>
+                            <td>{{ getBound(props.item.isOutbound) }}</td>
+                            <td>{{ props.item.roundTripTime }}</td>
+                        </template>
+                        <template v-slot:pageText="props">
+                            {{ props.pageStart }} - {{ props.pageStop }} of {{ props.itemsLength }}
+                        </template>
+                    </v-data-table>
+                </v-flex>
+            </v-layout>
+        </v-card-text>
+    </v-card>
 </template>
 
 <script>
@@ -56,22 +56,23 @@
         neighbors: state => state.node.neighbors,
       })
     },
-    data: () => ({
-      search: '',
-      pagination: {
-        descending: false,
-        rowsPerPage: 10,
-        sortBy: 'id'
-      },
-      headers: [
-        {text: 'ID', value: 'id', align: 'left', sortable: false},
-        {text: 'Address', value: 'addr', align: 'left', sortable: false},
-        {text: 'Sync state', value: 'syncState', align: 'left', sortable: false},
-        {text: 'Is outbound', value: 'isOutbound', align: 'left', sortable: false},
-        {text: 'Round Trip Time', value: 'roundTripTime', align: 'left', sortable: false}
-      ],
-
-    }),
+    data() {
+      return {
+        search: '',
+        pagination: {
+          descending: false,
+          rowsPerPage: 10,
+          sortBy: 'id'
+        },
+        headers: [
+          {text: this.$t('neighbor.header.ID'), value: 'id', align: 'left', sortable: false},
+          {text: this.$t('neighbor.header.IP'), value: 'addr', align: 'left', sortable: false},
+          {text: this.$t('neighbor.header.STATE'), value: 'syncState', align: 'left', sortable: false},
+          {text: this.$t('neighbor.header.BOUND'), value: 'isOutbound', align: 'left', sortable: false},
+          {text: this.$t('neighbor.header.PING'), value: 'roundTripTime', align: 'left', sortable: false}
+        ],
+      }
+    },
     methods: {
       getStatus(stateStr) {
         const statusEnum = {
@@ -82,6 +83,13 @@
           'PERSIST_FINISHED': this.$t('node.state.PERSIST_FINISHED')
         }
         return statusEnum[stateStr] || this.$t('node.state.DEFAULT')
+      },
+      getBound(str) {
+        const statusEnum = {
+          true: this.$t('neighbor.OUT_BOUND'),
+          false: this.$t('neighbor.IN_BOUND')
+        }
+        return statusEnum[str]
       },
     }
   }
--- dashboard/web/locales/en/neighbor.js
+++ dashboard/web/locales/en/neighbor.js
@@ -1,4 +1,16 @@
 export default {
   TITLE: 'Neighbors',
-  WARNING: 'Warning: No entry to the neighbor. Or the outside will not come in.'
+  WARNING: 'Warning: No entry to the neighbor. Or the outside will not come in.',
+
+  OUT_BOUND: 'out',
+  IN_BOUND: 'in',
+
+  header:{
+    ID: 'ID',
+    IP: 'IP',
+    STATE: 'State',
+    BOUND: 'Bound',
+    PING: 'Ping'
+  }
+
 }
