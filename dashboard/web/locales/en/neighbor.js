dda740ce4974c1e25dc3fc7f91f24acfd15db4d4
heron <peng.liu@nkn.org>
2019-07-18 16:22:52 +0800

    cleaning code and update language text
    Signed-off-by: heron <peng.liu@nkn.org>

--- dashboard/web/locales/en/neighbor.js
+++ dashboard/web/locales/en/neighbor.js
@@ -1,6 +1,6 @@
 export default {
   TITLE: 'Neighbors',
-  WARNING: 'Warning: No entry to the neighbor. Or the outside will not come in.',
+  WARNING: 'Warning: Local node has no inbound neighbor, it\'s possible that local node is unreachable from outside, e.g. behind firewall or NAT.',
 
   OUT_BOUND: 'out',
   IN_BOUND: 'in',
