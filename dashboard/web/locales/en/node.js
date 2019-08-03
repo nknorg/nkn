379e47374e4b8bcec41c07cdbde5e50a2a360eeb
heron <peng.liu@nkn.org>
2019-08-03 14:59:27 +0800

    1. add 301 / to /web 2. inBoundCount default is undefined 3. update language text
    Signed-off-by: heron <peng.liu@nkn.org>

--- dashboard/web/locales/en/node.js
+++ dashboard/web/locales/en/node.js
@@ -1,10 +1,10 @@
 export default {
   state: {
-    DEFAULT: 'not started.',
-    WAIT_FOR_SYNCING: 'wait for chain db syncing.',
-    SYNC_STARTED: 'chain db sync started.',
-    SYNC_FINISHED: 'chain db sync finished.',
-    PERSIST_FINISHED: 'mining.'
+    DEFAULT: 'not started',
+    WAIT_FOR_SYNCING: 'wait for chain db syncing',
+    SYNC_STARTED: 'chain db sync started',
+    SYNC_FINISHED: 'chain db sync finished',
+    PERSIST_FINISHED: 'mining'
   }
 
 }
