db42442dbd82a3e567881ddcf97c3c9d01c9c770
Yilun <zyl.skysniper@gmail.com>
2020-03-03 20:53:07 -0800

    docker entrypoint will not create wallet if --web-gui-create-wallet is enabled
    Signed-off-by: Yilun <zyl.skysniper@gmail.com>

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 set -e
 
-if echo "$1" | grep -q "^nknd"; then
+if [ "$1" == "nknd" ]; then
   ! [ -s web ] && echo "Copying web directory..." && cp -R ../web web
   ! [ -s certs ] && echo "Copying default certs..." && cp -R ../certs certs
   ! [ -s config.json ] && echo "Copying default config.json..." && cp /nkn/config.mainnet.json config.json
-  ! [ -s wallet.json ] && ! [ -s wallet.pswd ] && echo "Creating wallet.pswd..." && head -c 1024 /dev/urandom | tr -dc A-Za-z0-9 | head -c 32 > wallet.pswd && echo >> wallet.pswd
-  ! [ -s wallet.json ] && echo "Creating wallet.json..." && cat wallet.pswd wallet.pswd | nknc wallet -c
+  if echo "$@" | grep -q -v " --web-gui-create-wallet"; then
+    ! [ -s wallet.json ] && ! [ -s wallet.pswd ] && echo "Creating wallet.pswd..." && head -c 1024 /dev/urandom | tr -dc A-Za-z0-9 | head -c 32 > wallet.pswd && echo >> wallet.pswd
+    ! [ -s wallet.json ] && echo "Creating wallet.json..." && cat wallet.pswd wallet.pswd | nknc wallet -c
+  fi
 fi
 
 exec "$@"
