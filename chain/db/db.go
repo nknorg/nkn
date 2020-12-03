d25554afdf7d0f29fb2e5cddec3f70d4d611035d
Yilun <zyl.skysniper@gmail.com>
2020-12-02 17:41:00 -0800

    Adds per node leveldb config with randomized value
    This commit adds per node level db config with two purposes:
    
    1. Reduce compation time by disabling seek compaction, increase
       compaction trigger, table size and buffer size.
    2. Give each node a random compation trigger, table size and
       buffer size to make different node compact at different time
    
    Signed-off-by: Yilun <zyl.skysniper@gmail.com>

--- chain/db/db.go
+++ chain/db/db.go
@@ -1,7 +1,12 @@
 package db
 
 import (
+	"encoding/json"
+	"io/ioutil"
+	"math/rand"
+
 	"github.com/nknorg/nkn/v2/config"
+	"github.com/nknorg/nkn/v2/util/log"
 	"github.com/syndtr/goleveldb/leveldb"
 	"github.com/syndtr/goleveldb/leveldb/errors"
 	"github.com/syndtr/goleveldb/leveldb/filter"
@@ -28,24 +33,80 @@ type LevelDBStore struct {
 	batch *leveldb.Batch
 }
 
-// used to compute the size of bloom filter bits array .
-// too small will lead to high false positive rate.
-const BITSPERKEY = 10
+type LevelDBConfig struct {
+	CompactionL0Trigger    int
+	WriteL0SlowdownTrigger int
+	WriteL0PauseTrigger    int
+	CompactionTableSize    int
+	WriteBuffer            int
+	BlockCacheCapacity     int
+	DisableSeeksCompaction bool
+}
+
+const (
+	// used to compute the size of bloom filter bits array, too small will lead to
+	// high false positive rate.
+	BITSPERKEY     = 10
+	DBConfigSuffix = ".config"
+)
+
+func NewLevelDBConfig(file string) *LevelDBConfig {
+	c := &LevelDBConfig{}
+	if b, err := ioutil.ReadFile(file); err == nil {
+		if err = json.Unmarshal(b, c); err == nil {
+			return c
+		}
+	}
+
+	triggerRandFactor := 1 + rand.Float32()
+	sizeRandFactor := 1 + rand.Float32()
+
+	compactionL0Trigger := int(4 * triggerRandFactor)
+	compactionTableSize := int(2 * opt.MiB * sizeRandFactor)
+
+	c = &LevelDBConfig{
+		CompactionL0Trigger:    compactionL0Trigger,
+		WriteL0SlowdownTrigger: 2 * compactionL0Trigger,
+		WriteL0PauseTrigger:    3 * compactionL0Trigger,
+		CompactionTableSize:    compactionTableSize,
+		WriteBuffer:            2 * compactionTableSize,
+		BlockCacheCapacity:     4 * compactionTableSize,
+		DisableSeeksCompaction: true,
+	}
+
+	b, err := json.MarshalIndent(c, "", "    ")
+	if err != nil {
+		log.Errorf("Marshal leveldb opts error: %v", err)
+		return c
+	}
+
+	err = ioutil.WriteFile(file, b, 0666)
+	if err != nil {
+		log.Errorf("Save leveldb opts error: %v", err)
+		return c
+	}
+
+	return c
+}
 
 func NewLevelDBStore(file string) (*LevelDBStore, error) {
-	// default Options
-	o := opt.Options{
-		NoSync:                 false,
+	c := NewLevelDBConfig(file + DBConfigSuffix)
+	opts := &opt.Options{
+		CompactionL0Trigger:    c.CompactionL0Trigger,
+		WriteL0SlowdownTrigger: c.WriteL0SlowdownTrigger,
+		WriteL0PauseTrigger:    c.WriteL0PauseTrigger,
+		CompactionTableSize:    c.CompactionTableSize,
+		WriteBuffer:            c.WriteBuffer,
+		BlockCacheCapacity:     c.BlockCacheCapacity,
+		DisableSeeksCompaction: c.DisableSeeksCompaction,
 		OpenFilesCacheCapacity: int(config.Parameters.DBFilesCacheCapacity),
 		Filter:                 filter.NewBloomFilter(BITSPERKEY),
 	}
 
-	db, err := leveldb.OpenFile(file, &o)
-
+	db, err := leveldb.OpenFile(file, opts)
 	if _, corrupted := err.(*errors.ErrCorrupted); corrupted {
 		db, err = leveldb.RecoverFile(file, nil)
 	}
-
 	if err != nil {
 		return nil, err
 	}
--- go.mod
+++ go.mod
@@ -6,7 +6,7 @@ require (
 	github.com/gin-contrib/sessions v0.0.0-20190512062852-3cb4c4f2d615
 	github.com/gin-gonic/gin v1.4.0
 	github.com/go-acme/lego/v3 v3.8.0
-	github.com/golang/protobuf v1.4.1
+	github.com/golang/protobuf v1.4.2
 	github.com/gorilla/securecookie v1.1.1
 	github.com/gorilla/websocket v1.4.0
 	github.com/howeyc/gopass v0.0.0-20170109162249-bf9dde6d0d2c
@@ -19,8 +19,8 @@ require (
 	github.com/pbnjay/memory v0.0.0-20190104145345-974d429e7ae4
 	github.com/pborman/uuid v1.2.0
 	github.com/rdegges/go-ipify v0.0.0-20150526035502-2d94a6a86c40
-	github.com/syndtr/goleveldb v1.0.0
+	github.com/syndtr/goleveldb v1.0.1-0.20200815110645-5c35d600f0ca
 	github.com/urfave/cli v1.22.1
-	golang.org/x/crypto v0.0.0-20200510223506-06a226fb4e37
+	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9
 	golang.org/x/time v0.0.0-20191024005414-555d28b269f0
 )
