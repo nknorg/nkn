eaac1fd83d955316f26e408617dae06c40fbd9bf
Yilun <zyl.skysniper@gmail.com>
2022-04-02 02:16:41 -0700

    Update web dashboard config translation
    Signed-off-by: Yilun <zyl.skysniper@gmail.com>

--- dashboard/web/locales/en/config.js
+++ dashboard/web/locales/en/config.js
@@ -1,7 +1,7 @@
 export default {
-  Register_ID_Txn_Fee: 'Register ID txn fee',
+  Register_ID_Txn_Fee: 'Generate ID txn fee',
   Low_Fee_Txn_Size_Per_Block: 'Low fee txn size per block',
   Num_Low_Fee_Txn_Per_Block: 'Num low fee txn per block',
   Low_Txn_Fee: 'Low txn fee',
-  Low_Txn_Fee_Per_Size: 'Each Byte unit fee'
+  Low_Txn_Fee_Per_Size: 'Low txn fee per byte'
 }
--- dashboard/web/locales/zh/config.js
+++ dashboard/web/locales/zh/config.js
@@ -1,7 +1,7 @@
 export default {
-  Register_ID_Txn_Fee: '注册ID费用',
-  Low_Fee_Txn_Size_Per_Block: '低费率区块大小',
-  Num_Low_Fee_Txn_Per_Block: '低费率区块数量',
-  Low_Txn_Fee: '最小手续费',
-  Low_Txn_Fee_Per_Size: '每Byte手续费'
+  Register_ID_Txn_Fee: '注册ID交易费',
+  Low_Fee_Txn_Size_Per_Block: '低费交易大小',
+  Num_Low_Fee_Txn_Per_Block: '低费交易数量',
+  Low_Txn_Fee: '低费交易费',
+  Low_Txn_Fee_Per_Size: '低费交易每 byte 交易费'
 }
