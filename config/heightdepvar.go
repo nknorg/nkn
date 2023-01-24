fc8682e10185057e65821fbd80475ee30b68f54e
bufrr <chouyc.adam@gmail.com>
2023-01-24 11:20:52 +0800

    fix nameservice testcases

--- config/heightdepvar.go
+++ config/heightdepvar.go
@@ -3,70 +3,70 @@ package config
 import "github.com/nknorg/nkn/v2/common"
 
 type HeightDependentInt32 struct {
-	heights []uint32
-	values  []int32
+	Height []uint32
+	Values []int32
 }
 
 func (hdi *HeightDependentInt32) GetValueAtHeight(height uint32) int32 {
-	for i, h := range hdi.heights {
+	for i, h := range hdi.Height {
 		if height >= h {
-			return hdi.values[i]
+			return hdi.Values[i]
 		}
 	}
 	return 0
 }
 
 type HeightDependentInt64 struct {
-	heights []uint32
-	values  []int64
+	Height []uint32
+	Values []int64
 }
 
 func (hdi *HeightDependentInt64) GetValueAtHeight(height uint32) int64 {
-	for i, h := range hdi.heights {
+	for i, h := range hdi.Height {
 		if height >= h {
-			return hdi.values[i]
+			return hdi.Values[i]
 		}
 	}
 	return 0
 }
 
 type HeightDependentUint256 struct {
-	heights []uint32
-	values  []common.Uint256
+	Height []uint32
+	Values []common.Uint256
 }
 
 func (hdi *HeightDependentUint256) GetValueAtHeight(height uint32) common.Uint256 {
-	for i, h := range hdi.heights {
+	for i, h := range hdi.Height {
 		if height >= h {
-			return hdi.values[i]
+			return hdi.Values[i]
 		}
 	}
 	return common.EmptyUint256
 }
 
 type HeightDependentBool struct {
-	heights []uint32
-	values  []bool
+	Heights []uint32
+	Values  []bool
 }
 
 func (hdi *HeightDependentBool) GetValueAtHeight(height uint32) bool {
-	for i, h := range hdi.heights {
+	for i, h := range hdi.Heights {
 		if height >= h {
-			return hdi.values[i]
+			return hdi.Values[i]
 		}
 	}
 	return false
 }
 
 type HeightDependentString struct {
-	heights []uint32
-	values  []string
+	Height []uint32
+	Values []string
 }
 
 func (hdi *HeightDependentString) GetValueAtHeight(height uint32) string {
-	for i, h := range hdi.heights {
+	for i, h := range hdi.Height {
 		if height >= h {
-			return hdi.values[i]
+			return hdi.Values[i]
 		}
 	}
 	return ""
